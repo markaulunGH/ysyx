250dd25cbcd0319a77c14ff9ce35c353fa6952be
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-01 09:54:19 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.79.1-microsoft-standard-WSL2 #1 SMP Wed Nov 23 01:01:46 UTC 2022 x86_64 x86_64 x86_64 GNU/Linux  09:54:19 up 13 min,  0 users,  load average: 0.03, 0.01, 0.00

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -25,7 +25,7 @@ const char *regs[] = {
 
 void isa_reg_display() {
   for (int i = 0; i < 32; ++ i) {
-    printf("%s\t\t0x%x\t\t%d\n", gpr[i], gpr[i]);
+    printf("%s\t\t0x%x\t\t%d\n", regs[i], gpr[i], gpr[i]);
   }
 }
 
