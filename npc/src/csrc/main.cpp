ff1da29adf6f2595f92c100a9a2613655adf22ad
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-04 19:14:08 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:14:08 up  1:30,  0 users,  load average: 0.54, 0.38, 0.20

--- npc/src/csrc/main.cpp
+++ npc/src/csrc/main.cpp
@@ -19,13 +19,13 @@ int main(int argc, char** argv, char** env)
     top->trace(tfp, 0);
     tfp->open("logs/dump.fst");
 
-    // nvboard_bind_pin(&top->io_led, BIND_RATE_SCR, BIND_DIR_OUT, 16, LD15, LD14, LD13, LD12, LD11, LD10, LD9, LD8, LD7, LD6, LD5, LD4, LD3, LD2, LD1, LD0);
-    // nvboard_init();
+    nvboard_bind_pin(&top->io_led, BIND_RATE_SCR, BIND_DIR_OUT, 16, LD15, LD14, LD13, LD12, LD11, LD10, LD9, LD8, LD7, LD6, LD5, LD4, LD3, LD2, LD1, LD0);
+    nvboard_init();
 
     while (contextp->time() < 100000000)
     {
         contextp->timeInc(1);
-        // nvboard_update();
+        nvboard_update();
 
         top->reset = contextp->time() < 100;
 
