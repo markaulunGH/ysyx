08d981838151398ba5de5b0446f924d0fe1d35bc
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-04 18:27:18 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:27:18 up 43 min,  0 users,  load average: 0.12, 0.17, 0.11

--- npc/src/csrc/main.cpp
+++ npc/src/csrc/main.cpp
@@ -1,97 +1,39 @@
-// For std::unique_ptr
 #include <memory>
-
-// Include common routines
 #include <verilated.h>
-
-// Include model header, generated from Verilating "top.v"
 #include "Vtop.h"
-
 #include "verilated_fst_c.h"
-
-//generate random numbers in 0~x
-int random(int x)
-{
-    return rand() % x;
-}
 
 int main(int argc, char** argv, char** env)
 {
-    // Create logs/ directory in case we have traces to put under it
     Verilated::mkdir("logs");
 
-    // Construct a VerilatedContext to hold simulation time, etc.
-    // Multiple modules (made later below with Vtop) may share the same
-    // context to share time, or modules may have different contexts if
-    // they should be independent from each other.
-
-    // Using unique_ptr is similar to
-    // "VerilatedContext* contextp = new VerilatedContext" then deleting at end.
     const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
-
-    // Set debug level, 0 is off, 9 is highest presently used
-    // May be overridden by commandArgs argument parsing
     contextp->debug(0);
-
-    // Randomization reset policy
-    // May be overridden by commandArgs argument parsing
     contextp->randReset(2);
-
-    // Verilator must compute traced signals
     contextp->traceEverOn(true);
-
-    // Pass arguments so Verilated code can see them, e.g. $value$plusargs
-    // This needs to be called before you create any model
     contextp->commandArgs(argc, argv);
 
-    // Construct the Verilated model, from Vtop.h generated from Verilating "top.v".
-    // Using unique_ptr is similar to "Vtop* top = new Vtop" then deleting at end.
-    // "TOP" will be the hierarchical name of the module.
     const std::unique_ptr<Vtop> top{new Vtop{contextp.get(), "TOP"}};
     VerilatedFstC* tfp = new VerilatedFstC;
     top->trace(tfp, 0);
     tfp->open("logs/dump.fst");
 
-    // Simulate until $finish
     while (!contextp->gotFinish() && contextp->time() < 100)
     {
-        // Historical note, before Verilator 4.200 Verilated::gotFinish()
-        // was used above in place of contextp->gotFinish().
-        // Most of the contextp-> calls can use Verilated:: calls instead;
-        // the Verilated:: versions simply assume there's a single context
-        // being used (per thread).  It's faster and clearer to use the
-        // newer contextp-> versions.
+        contextp->timeInc(1);
 
-        contextp->timeInc(1);  // 1 timeprecision period passes...
-        // Historical note, before Verilator 4.200 a sc_time_stamp()
-        // function was required instead of using timeInc.  Once timeInc()
-        // is called (with non-zero), the Verilated libraries assume the
-        // new API, and sc_time_stamp() will no longer work.
-
-        int in = random(1 << 4);
-        top->io_in = in;
-
-        // Evaluate model
-        // (If you have multiple models being simulated in the same
-        // timestep then instead of eval(), call eval_step() on each, then
-        // eval_end_step() on each. See the manual.)
         top->eval();
-        assert(top->io_out == in);
 
         tfp->dump(contextp->time());
     }
 
-    // Final model cleanup
     top->final();
     tfp->close();
 
-    // Coverage analysis (calling write only after the test is known to pass)
 #if VM_COVERAGE
     Verilated::mkdir("logs");
     contextp->coveragep()->write("logs/coverage.dat");
 #endif
 
-    // Return good completion status
-    // Don't use exit() or destructor won't get called
     return 0;
 }
