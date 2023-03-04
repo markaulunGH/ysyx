7bd7311de75a22bb0171a9cc06e7fcb05d04257a
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-04 18:12:39 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:12:39 up 29 min,  0 users,  load average: 0.00, 0.00, 0.00

---
+++ npc/src/src/Light.scala
@@ -0,0 +1,28 @@
+import chisel3._
+
+class top extends Module
+{
+    val io = IO(new Bundle
+    {
+        val led = Output(Reg(UInt(16.W)))
+    })
+
+    val count = RegInit(0.U(32.W))
+    val led = RegInit(1.U(16.W))
+
+    when (count == 0)
+    {
+        led := Cat(led(14, 0), led(15))
+    }
+
+    when (count >= 5000000)
+    {
+        count := 0.U(32.W)
+    }
+    .otherwise
+    {
+        count := count + 1
+    }
+
+    io.led := led
+}
