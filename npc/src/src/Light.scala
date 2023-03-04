22cff7797b35d27c90596d1e3a72d32f11a84775
ysyx_22051094-Li Yanjun <liyanjun20@mails.ucas.ac.cn>
2023-03-04 19:18:51 +0800

    connect to nvboard

---
+++ npc/src/src/Light.scala
@@ -0,0 +1,21 @@
+import chisel3._
+import chisel3.util._
+
+class top extends Module
+{
+    val io = IO(new Bundle
+    {
+        val led = Output(UInt(16.W))
+    })
+
+    val count = RegInit(0.U(32.W))
+    val led = RegInit(1.U(16.W))
+    
+    count := Mux(count >= 5000000.U, 0.U, count + 1.U)
+    when (count === 0.U(32.W))
+    {
+        led := Cat(led(14, 0), led(15))
+    }
+
+    io.led := led
+}
