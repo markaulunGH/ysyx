53d23d4e49ae689898eccf1ed98c5cd447cd7b2f
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-05 10:24:49 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:24:49 up 49 min,  0 users,  load average: 0.22, 0.14, 0.06

--- npc/src/src/Keyboard.scala
+++ npc/src/src/Keyboard.scala
@@ -35,7 +35,7 @@ class top extends Module
         when (io.nextdata_n === 0.U)
         {
             r_ptr := r_ptr + 1.U
-            when (w_ptr === r_ptr + 1.U)
+            when (w_ptr === (r_ptr + 1.U))
                 ready := 0.U
         }
     }
