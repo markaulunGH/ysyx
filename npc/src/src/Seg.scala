746295b825e5ec72880e6689c0c19439090792c2
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-05 23:44:51 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:44:51 up  6:46,  0 users,  load average: 0.49, 0.63, 0.46

--- npc/src/src/Seg.scala
+++ npc/src/src/Seg.scala
@@ -49,7 +49,7 @@ class Seg extends Module
     seg1.io.num := RegEnable(io.data(7, 4), io.ready.asBool)
     io.seg1 := seg1.io.seg
 
-    io.seg2 := 0x71.U
+    io.seg2 := 0xff.U
     io.seg3 := 0xff.U
     io.seg4 := 0xff.U
     io.seg5 := 0xff.U
