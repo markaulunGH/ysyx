8d68395ae558335f444a93a43b8fe5cf803575c7
ysyx_22051094-Li Yanjun <liyanjun20@mails.ucas.ac.cn>
2023-03-05 23:46:26 +0800

    complete keyboard

--- npc/src/csrc/main.cpp
+++ npc/src/csrc/main.cpp
@@ -19,7 +19,17 @@ int main(int argc, char** argv, char** env)
     top->trace(tfp, 0);
     tfp->open("logs/dump.fst");
 
-    nvboard_bind_pin(&top->io_led, BIND_RATE_SCR, BIND_DIR_OUT, 16, LD15, LD14, LD13, LD12, LD11, LD10, LD9, LD8, LD7, LD6, LD5, LD4, LD3, LD2, LD1, LD0);
+    nvboard_bind_pin(&top->io_ps2_clk, BIND_RATE_RT , BIND_DIR_IN , 1, PS2_CLK);
+	nvboard_bind_pin(&top->io_ps2_data, BIND_RATE_RT , BIND_DIR_IN , 1, PS2_DAT);
+    nvboard_bind_pin(&top->io_seg0, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG0A, SEG0B, SEG0C, SEG0D, SEG0E, SEG0F, SEG0G, DEC0P);
+    nvboard_bind_pin(&top->io_seg1, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG1A, SEG1B, SEG1C, SEG1D, SEG1E, SEG1F, SEG1G, DEC1P);
+    nvboard_bind_pin(&top->io_seg2, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG2A, SEG2B, SEG2C, SEG2D, SEG2E, SEG2F, SEG2G, DEC2P);
+    nvboard_bind_pin(&top->io_seg3, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG3A, SEG3B, SEG3C, SEG3D, SEG3E, SEG3F, SEG3G, DEC3P);
+    nvboard_bind_pin(&top->io_seg4, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG4A, SEG4B, SEG4C, SEG4D, SEG4E, SEG4F, SEG4G, DEC4P);
+    nvboard_bind_pin(&top->io_seg5, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG5A, SEG5B, SEG5C, SEG5D, SEG5E, SEG5F, SEG5G, DEC5P);
+    nvboard_bind_pin(&top->io_seg6, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG6A, SEG6B, SEG6C, SEG6D, SEG6E, SEG6F, SEG6G, DEC6P);
+    nvboard_bind_pin(&top->io_seg7, BIND_RATE_SCR, BIND_DIR_OUT, 8, SEG7A, SEG7B, SEG7C, SEG7D, SEG7E, SEG7F, SEG7G, DEC7P);
+
     nvboard_init();
 
     while (1)
---
+++ npc/src/src/Seg.scala
@@ -0,0 +1,58 @@
+import chisel3._
+import chisel3.util._
+
+class hex2seg extends Module
+{
+    val io = IO(new Bundle
+    {
+        val num = Input(UInt(4.W))
+        val seg = Output(UInt(8.W))
+    })
+
+    val lut = List(0x3, 0x9f, 0x25, 0xd, 0x99, 0x49, 0x41, 0x1f, 0x1, 0x9, 0x11, 0xc1, 0x63, 0x85, 0x61, 0x71)
+
+    io.seg := 0xff.U
+    for (i <- 0 until 16)
+    {
+        when (io.num === i.U)
+        {
+            io.seg := lut(i).U
+        }
+    }
+}
+
+class Seg extends Module
+{
+    val io = IO(new Bundle
+    {
+        val data = Input(UInt(8.W))
+        val ready = Input(UInt(1.W))
+        val overflow = Input(UInt(1.W))
+        val nextdata_n = Output(UInt(1.W))
+        val seg0 = Output(UInt(8.W))
+        val seg1 = Output(UInt(8.W))
+        val seg2 = Output(UInt(8.W))
+        val seg3 = Output(UInt(8.W))
+        val seg4 = Output(UInt(8.W))
+        val seg5 = Output(UInt(8.W))
+        val seg6 = Output(UInt(8.W))
+        val seg7 = Output(UInt(8.W))
+    })
+
+    io.nextdata_n := RegNext(~io.ready)
+
+    val seg0 = Module(new hex2seg)
+    seg0.io.num := RegEnable(io.data(3, 0), io.ready.asBool)
+    io.seg0 := seg0.io.seg
+
+    val seg1 = Module(new hex2seg)
+    seg1.io.num := RegEnable(io.data(7, 4), io.ready.asBool)
+    io.seg1 := seg1.io.seg
+
+    io.seg2 := 0xff.U
+    io.seg3 := 0xff.U
+    io.seg4 := 0xff.U
+    io.seg5 := 0xff.U
+    io.seg6 := 0xff.U
+    io.seg7 := 0xff.U
+}
