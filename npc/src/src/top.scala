62d12fbf4875ec2f55007f56b90be6a9684ac709
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-05 20:51:45 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:51:45 up  3:52,  0 users,  load average: 0.37, 0.17, 0.06

--- npc/src/src/top.scala
+++ npc/src/src/top.scala
@@ -49,9 +49,9 @@ class top extends  Module {
     val lut = List(0x3f, 0x06, 0x5b, 0x4f, 0x66, 0x6d, 0x7d, 0x07, 0x7f, 0x6f, 0x77, 0x7c, 0x39, 0x5e, 0x79, 0x71)
 
     io.seg := 0.U
-    for (i <- 0 until 16)
+    for (i <- 0 until 15)
     {
-        if (io.num === i)
+        when (io.num === i)
         {
             io.seg := lut(i).U
         }
