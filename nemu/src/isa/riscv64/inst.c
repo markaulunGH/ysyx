c390ee91652db89a6a06caeda7ceceefafb3d365
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-06 16:20:18 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:20:18 up  1:02,  0 users,  load average: 0.15, 0.10, 0.03

--- nemu/src/isa/riscv64/inst.c
+++ nemu/src/isa/riscv64/inst.c
@@ -45,6 +45,7 @@ static void decode_operand(Decode *s, int *dest, word_t *src1, word_t *src2, wor
     case TYPE_R: src1R(); src2R();         break;
     case TYPE_I: src1R();          immI(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
+    case TYPE_B: src1R(); src2R(); immB(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_J:                   immJ(); break;
   }
