f2983ff762370a1e07452dab7130bf3b7a72a4f6
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-07 15:02:51 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:02:51 up 1 min,  0 users,  load average: 0.85, 0.40, 0.15

--- npc/src/src/top.scala
+++ npc/src/src/top.scala
@@ -15,12 +15,13 @@ class top extends Module
     pc := pc + 4.U
     io.pc := pc
     
-    val decoder7128 = Module(new decoder(7, 128))
-    val decoder38 = Module(new decoder(3, 8))
-    decoder7128.io.in := io.inst(6, 0)
-    decoder38.io.in := io.inst(14, 12)
+    // val decoder7128 = Module(new decoder(7, 128))
+    // val decoder38 = Module(new decoder(3, 8))
+    // decoder7128.io.in := io.inst(6, 0)
+    // decoder38.io.in := io.inst(14, 12)
+    // val inst_addi = decoder7128.io.out(0x13) & decoder38.io.out(0x0)
     
-    val inst_addi = decoder7128.io.out(0x13) & decoder38.io.out(0x0)
+    val inst_addi = inst(6, 0) === 0x13 & inst(14, 12) === 0x0
     
     val rf = Module(new regfile)
     val rs1 = io.inst(19, 15)
