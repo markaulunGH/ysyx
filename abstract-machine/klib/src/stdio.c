b44c841c720b8ec3fc865480809769de19e75c15
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-09 18:08:00 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:08:00 up  3:36,  0 users,  load average: 0.25, 0.07, 0.02

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -2,7 +2,6 @@
 #include <klib.h>
 #include <klib-macros.h>
 #include <stdarg.h>
-#include <stdlib.h>
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
@@ -15,38 +14,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  va_list arg;
-
-  va_start (arg, fmt);
-  for (; *fmt; ++ fmt) {
-    if (*fmt == '%') {
-      ++ fmt;
-      if (*fmt == 'd') {
-        int d = va_arg(arg, int);
-        char tmp[20];
-        int ptr = 0;
-        while (d) {
-          tmp[ptr ++] = d % 10;
-          d /= 10;
-        }
-        while (ptr) {
-          *out ++ = tmp[-- ptr];
-        }
-      }
-      else if (*fmt == 's') {
-        char *s = va_arg(arg, char *);
-        while (*s) {
-          *out ++ = *s ++;
-        }
-      }
-    }
-    else {
-      *out ++ = *fmt;
-    }
-  }
-  va_end (arg);
-
-  return 1;
+  panic("Not implemented");
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
