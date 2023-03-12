32cf18a34383001d78b5dd9a4628d1b26b23670d
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-12 22:35:59 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:35:59 up  1:22,  0 users,  load average: 0.25, 0.12, 0.04

--- npc/src/src/EX.scala
+++ npc/src/src/EX.scala
@@ -6,7 +6,7 @@ class EX extends Module
     val io = IO(new Bundle
     {
         val ID_EX = Flipped(new ID_EX)
-        val EX_MM = Flipped(new EX_MM)
+        val EX_MM = new EX_MM
     })
     val alu = Module(new alu)
     io.ID_EX.alu <> alu.io.in
