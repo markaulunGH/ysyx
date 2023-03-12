61c3e6b6ca0763996d0441c277139d02fb5ab0ce
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-12 22:52:51 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:52:51 up  1:39,  0 users,  load average: 0.13, 0.06, 0.07

--- npc/src/src/alu.scala
+++ npc/src/src/alu.scala
@@ -1,21 +1,26 @@
 import chisel3._
 import chisel3.util._
 
+class alu_in extends Bundle
+{
+    val alu_op = Input(Vec(19, UInt(1.W)))
+    val alu_src1 = Input(UInt(64.W))
+    val alu_src2 = Input(UInt(64.W))
+}
+
 class alu extends Module
 {
     val io = IO(new Bundle
     {
-        val alu_op = Input(Vec(19, UInt(1.W)))
-        val alu_src1 = Input(UInt(64.W))
-        val alu_src2 = Input(UInt(64.W))
+        val in = new alu_in
         val alu_result = Output(UInt(64.W))
     })
 
-    val op_add = io.alu_op(0)
+    val op_add = io.in.alu_op(0)
 
     when (op_add === 1.U)
     {
-        io.alu_result := io.alu_src1 + io.alu_src2
+        io.alu_result := io.in.alu_src1 + io.in.alu_src2
     }
     .otherwise
     {
