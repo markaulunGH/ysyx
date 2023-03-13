0f1494eae39ba4549f64ec30daa841da8dd31c2b
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-13 21:05:59 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:05:59 up  9:36,  0 users,  load average: 0.20, 0.14, 0.10

--- npc/src/src/DS.scala
+++ npc/src/src/DS.scala
@@ -11,7 +11,7 @@ class DS extends Module
         val reg_r = Flipped(new reg_r)
 
         val ebreak = Output(Bool())
-        // val tmp = Output(UInt(64.W))
+        val tmp = Output(UInt(64.W))
     })
 
     val inst = io.fs_ds.inst
@@ -85,7 +85,7 @@ class DS extends Module
     {
         io.fs_ds.br_target := 0.U
     }
-    // io.tmp := io.reg_r.rdata1 & ~1.U
+    io.tmp := Cat(io.reg_r.rdata1(63, 1), 0.U(1.W)
 
     io.reg_r.raddr1 := Mux(inst_lui, 0.U, rs1)
     io.reg_r.raddr2 := rs2
