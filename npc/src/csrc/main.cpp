60ed5e300f28faa494466c38f73166986bfa3b4f
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-13 12:26:04 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:26:04 up  2:36,  0 users,  load average: 0.01, 0.02, 0.00

--- npc/src/csrc/main.cpp
+++ npc/src/csrc/main.cpp
@@ -69,6 +69,7 @@ void end_simulation()
 int main(int argc, char** argv, char** env)
 {
     load_image(argv[2]);
+    printf("load image success\n");
     init_simulation(argc, argv);
     while (1)
     {
