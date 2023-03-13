4fa0ed83a95cc2faaa979a6fc30260b5ac5e5f99
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-13 15:52:43 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:52:43 up  4:23,  0 users,  load average: 0.04, 0.09, 0.04

--- npc/src/src/Tools.scala
+++ npc/src/src/Tools.scala
@@ -6,10 +6,10 @@ class Decoder(inWidth: Int, outWidth: Int) extends Module
     val io = IO(new Bundle
     {
         val in = Input(UInt(inWidth.W))
-        val out = Output(Bool(outWidth.W))
+        val out = Output(Vec(outWidth, Bool()))
     })
 
-    val out = Wire(Vec(outWidth, UInt(1.W)))
+    // val out = Wire(Vec(outWidth, UInt(1.W)))
     for (i <- 0 until outWidth)
     {
         out(i) := io.in === i.U
