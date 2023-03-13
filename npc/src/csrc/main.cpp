87a2c4b031e8f6521a9d491614509b48a2afa6a1
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-13 12:22:21 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:22:21 up  2:32,  0 users,  load average: 0.10, 0.04, 0.01

--- npc/src/csrc/main.cpp
+++ npc/src/csrc/main.cpp
@@ -4,49 +4,59 @@
 #include "verilated_fst_c.h"
 #include <nvboard.h>
 
-int main(int argc, char** argv, char** env)
+const int SIZE = 1 << 20;
+const long long offset = 0x80000000l;
+
+uint8_t img[SIZE];
+
+void load_image(char *img_file)
+{
+    FILE *fp = fopen(img_file, "rb");
+    fseek(fp, 0, SEEK_END);
+    int size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    fread(img, size, 1, fp);
+    fclose(fp);
+}
+
+uint32_t ifetch(uint64_t pc)
+{
+    return *(uint32_t*) (img + pc - offset);
+}
+
+const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+const std::unique_ptr<VTop> top{new VTop{contextp.get(), "TOP"}};
+VerilatedFstC* tfp = new VerilatedFstC;
+
+void init_simulation()
 {
     Verilated::mkdir("logs");
 
-    const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
     contextp->debug(0);
     contextp->randReset(2);
     contextp->traceEverOn(true);
     contextp->commandArgs(argc, argv);
 
-    const std::unique_ptr<VTop> top{new VTop{contextp.get(), "TOP"}};
-    VerilatedFstC* tfp = new VerilatedFstC;
     top->trace(tfp, 0);
     tfp->open("logs/dump.fst");
+}
 
-    int cnt = 0;
-    while (1)
-    {
-        contextp->timeInc(1);
-        top->reset = contextp->time() <= 100;
-        top->clock = ~top->clock;
+void cycle_begin()
+{
+    contextp->timeInc(1);
+    top->clock = 0;
+    contextp->timeInc(1);
+    top->clock = 1;
+    top->eval();
+}
 
-        top->eval();
+void cycle_end()
+{
+    tfp->dump(contextp->time());
+}
 
-        if (top->clock)
-        {
-            if (top->io_pc == 0x80000000 || top->reset)
-            {
-                top->io_inst = 0x100513;
-            }
-            else if (cnt < 1000)
-            {
-                top->io_inst = 0x150513;
-                ++ cnt;
-            }
-            else break;
-        }
-
-        top->eval();
-
-        tfp->dump(contextp->time());
-    }
-
+void end_simulation()
+{
     top->final();
     tfp->close();
 
@@ -54,6 +64,18 @@ int main(int argc, char** argv, char** env)
     Verilated::mkdir("logs");
     contextp->coveragep()->write("logs/coverage.dat");
 #endif
+}
 
-    return 0;
+int main(int argc, char** argv, char** env)
+{
+    load_image(argv[2]);
+    init_simulation();
+    while (1)
+    {
+        cycle_begin();
+        top->reset = contextp->time() < 100;
+        top->io_inst = ifetch(top->io_pc);
+        cycle_end();
+    }
+    end_simulation();
 }
