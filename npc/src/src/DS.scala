0163e2e962cf9a5ac601e3c5aa0d6901c9313be9
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-13 15:49:34 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:49:34 up  4:20,  0 users,  load average: 0.08, 0.06, 0.02

--- npc/src/src/DS.scala
+++ npc/src/src/DS.scala
@@ -31,22 +31,38 @@ class DS extends Module
     decoder38.io.in := funct3
     val dfunct3 = decoder38.io.out
 
+    val inst_lui   = dopcode(0x37)
     val inst_auipc = dopcode(0x17)
-    val inst_addi = dopcode(0x13) & dfunct3(0x0)
+    val inst_addi  = dopcode(0x13) & dfunct3(0x0)
+    val inst_jal   = dopcode(0x6f)
+    val inst_jalr  = dopcode(0x67)
 
     val src1_is_pc = inst_auipc
-    val src2_is_imm = inst_auipc || inst_addi
+    val src2_is_imm = inst_auipc || inst_addi || inst_lui
 
-    io.reg_r.raddr1 := inst(19, 15)
-    io.reg_r.raddr2 := inst(24, 20)
+    val imm = Wire(UInt(64.W))
+
+    .when (inst_jalr || inst_addi)
+        imm := imm_I
+    // .elsewhen ()
+    //     imm := imm_S
+    // .elsewhen ()
+    //     imm := imm_B
+    .elsewhen (inst_lui || inst_auipc)
+        imm := imm_U
+    .elsewhen (inst_jal)
+        imm := imm_J
+    .otherwise
+        imm := 0.U(64.W)
+
+    io.reg_r.raddr1 := Mux(inst_lui, 0, rs1)
+    io.reg_r.raddr2 := rs2
 
     for (i <- 0 until 19)
-    {
         io.ds_es.alu.alu_op(i) := 0.U
-    }
-    io.ds_es.alu.alu_op(0) := inst_auipc || inst_addi
+    io.ds_es.alu.alu_op(0) := inst_auipc || inst_addi || inst_lui
     io.ds_es.alu.alu_src1 := Mux(src1_is_pc, io.fs_ds.pc, io.reg_r.rdata1)
-    io.ds_es.alu.alu_src2 := Mux(src2_is_imm, inst(31, 20), io.reg_r.rdata2)
+    io.ds_es.alu.alu_src2 := Mux(src2_is_imm, imm, io.reg_r.rdata2)
 
     io.ds_es.wen := inst_addi
     io.ds_es.waddr := rd
