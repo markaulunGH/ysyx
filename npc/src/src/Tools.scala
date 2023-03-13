e04a063093ef049584e14a740d4fd725d0d0358d
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-13 15:51:07 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:51:07 up  4:21,  0 users,  load average: 0.18, 0.13, 0.05

--- npc/src/src/Tools.scala
+++ npc/src/src/Tools.scala
@@ -1,12 +1,12 @@
 import chisel3._
 import chisel3.util._
 
-class Decoder(inWidth: Int, outWidth: Bool) extends Module
+class Decoder(inWidth: Int, outWidth: Int) extends Module
 {
     val io = IO(new Bundle
     {
         val in = Input(UInt(inWidth.W))
-        val out = Output(UInt(outWidth.W))
+        val out = Output(Bool(outWidth.W))
     })
 
     val out = Wire(Vec(outWidth, UInt(1.W)))
