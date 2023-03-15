f0870fce38b8a54af98c21703a89ef2dbd0d14b3
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-15 19:54:15 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:54:15 up  9:52,  0 users,  load average: 0.27, 0.21, 0.09

--- npc/src/csrc/cpu/cpu-exec.cpp
+++ npc/src/csrc/cpu/cpu-exec.cpp
@@ -138,6 +138,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc)
 
 static void exec_once(Decode *s)
 {
+    printf("fxxk\n");
     s->pc = top->io_pc;
     cycle_begin();
     s->npc.inst.val = top->io_inst = paddr_read(top->io_pc, 4);
