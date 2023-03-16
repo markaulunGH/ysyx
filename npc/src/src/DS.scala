497f2498c16fac70aad48f8d0ef4fdfd4ad730ff
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-16 21:20:04 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:20:04 up 23 min,  0 users,  load average: 0.24, 0.25, 0.11

--- npc/src/src/DS.scala
+++ npc/src/src/DS.scala
@@ -109,7 +109,8 @@ class DS extends Module
     
     imm := MuxCase(
         0.U(64.W),
-        Array((inst_jal || inst_addi) -> Cat(Fill(52, imm_I(11)), imm_I),
+        Array(
+            inst_jal || inst_addi -> Cat(Fill(52, imm_I(11)), imm_I),
             // False.Bool -> Cat(Fill(52, imm_S(11), imm_S)),
             // False -> Cat(Fill(51, imm_B(12), imm_B)),
             (inst_lui || inst_auipc) -> Cat(Fill(32, imm_U(31)), imm_U),
