65ee751ef6756b3410c0398f00487f139af1bf63
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-16 09:37:31 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:37:31 up  1:40,  0 users,  load average: 0.03, 0.06, 0.04

--- npc/src/csrc/include/difftest.h
+++ npc/src/csrc/include/difftest.h
@@ -10,7 +10,7 @@ extern void (*ref_difftest_regcpy)(void *dut, bool direction);
 extern void (*ref_difftest_exec)(uint64_t n);
 extern void (*ref_difftest_raise_intr)(word_t NO);
 
-void init_difftest(char *reg_so_file, int img_size);
+void init_difftest(char *ref_so_file, int img_size);
 void difftest_step(vaddr_t pc, vaddr_t next_pc);
 void difftest_skip_ref();
 
