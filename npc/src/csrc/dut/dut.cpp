c8ee2aeb0b05d3e230276e6eaea1ff4cf6282739
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-16 09:47:28 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:47:28 up  1:50,  0 users,  load average: 0.06, 0.03, 0.02

--- npc/src/csrc/dut/dut.cpp
+++ npc/src/csrc/dut/dut.cpp
@@ -5,10 +5,10 @@
 #include <sdb.h>
 #include <difftest.h>
 
-const void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
-const void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
-const void (*ref_difftest_exec)(uint64_t n) = NULL;
-const void (*ref_difftest_raise_intr)(word_t NO) = NULL;
+void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
+void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
+void (*ref_difftest_exec)(uint64_t n) = NULL;
+void (*ref_difftest_raise_intr)(word_t NO) = NULL;
 
 static bool is_skip_ref = false;
 static int skip_dut_nr_inst = 0;
@@ -36,7 +36,7 @@ void init_difftest(char *ref_so_file, int img_size)
     // ref_difftest_regcpy = dlsym(handle, "difftest_regcpy");
     // ref_difftest_exec = dlsym(handle, "difftest_exec");
     // ref_difftest_raise_intr = dlsym(handle, "difftest_raise_intr");
-    const void (*ref_difftest_init)() = dlsym(handle, "difftest_init");
+    void (*ref_difftest_init)() = dlsym(handle, "difftest_init")();
 
     Log("Differential testing: %s", ANSI_FMT("ON", ANSI_FG_GREEN));
     Log("The result of every instruction will be compared with %s. "
