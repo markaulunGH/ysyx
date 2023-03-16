58a303ac28f3754b0132308b29147a5a4196ff11
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-16 11:29:31 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:29:31 up  3:32,  0 users,  load average: 0.13, 0.09, 0.02

--- npc/src/csrc/sim/sim.cpp
+++ npc/src/csrc/sim/sim.cpp
@@ -28,7 +28,7 @@ void reset()
     for (int i = 0; i < 100; ++ i)
     {
         cycle_begin();
-        top->reset = i < 100;
+        top->reset = i < 99;
         cycle_end();
     }
     // top->reset = 0;
