1947aafbccad2b0fddd01a3523462aa9a7581758
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-16 10:49:58 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:49:58 up  2:53,  0 users,  load average: 0.00, 0.03, 0.00

--- npc/src/csrc/main.cpp
+++ npc/src/csrc/main.cpp
@@ -43,6 +43,7 @@ int main(int argc, char** argv, char** env)
     init_disasm("riscv64-pc-linux-gnu");
 #endif
     init_simulation(2, argv);
+    update_regs();
 #ifdef CONFIG_DIFFTEST
     init_difftest(argv[diff_so_file] + 7, img_size);
 #endif
