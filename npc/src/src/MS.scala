8b2ce337a2903f56e29fabe4636e02d777c2c2e8
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-17 10:54:55 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:54:55 up  1:08,  0 users,  load average: 0.14, 0.14, 0.06

--- npc/src/src/MS.scala
+++ npc/src/src/MS.scala
@@ -5,13 +5,13 @@ class MS extends Module
 {
     val io = IO(new Bundle
     {
-        val es_mm = Flipped(new ES_MS)
+        val es_ms = Flipped(new ES_MS)
         val ms_ws = new MS_WS
 
         val mm_rdata = Input(UInt(64.W))
     })
 
-    io.ms_ws.rf_wen := io.es_mm.rf_wen
-    io.ms_ws.rf_waddr := io.es_mm.rf_waddr
-    io.ms_ws.rf_wdata := Mux(io.es_mm.res_from_mem, io.mm_rdata, io.es_mm.alu_result)
+    io.ms_ws.rf_wen := io.es_ms.rf_wen
+    io.ms_ws.rf_waddr := io.es_ms.rf_waddr
+    io.ms_ws.rf_wdata := Mux(io.es_ms.res_from_mem, io.mm_rdata, io.es_ms.alu_result)
 }
