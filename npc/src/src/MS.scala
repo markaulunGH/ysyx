cbbf34c51814eadbfa77ce252ef72ca5bea20935
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-17 11:34:02 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:34:02 up  1:48,  0 users,  load average: 0.20, 0.09, 0.03

--- npc/src/src/MS.scala
+++ npc/src/src/MS.scala
@@ -14,10 +14,10 @@ class MS extends Module
     val mm_rdata = MuxCase(
         0.U(64.W),
         Seq(
-            es_ms.mm_mask === 0x1.U  -> Cat(Fill(Mux(io.es_ms.mm_unsigned, 0.U(1.W), io.mm_rdata(7)), 56),  io.mm_rdata(7, 0)),
-            es_ms.mm_mask === 0x3.U  -> Cat(Fill(Mux(io.es_ms.mm_unsigned, 0.U(1.W), io.mm_rdata(15)), 48), io.mm_rdata(15, 0)),
-            es_ms.mm_mask === 0xf.U  -> Cat(Fill(Mux(io.es_ms.mm_unsigned, 0.U(1.W), io.mm_rdata(31)), 32), io.mm_rdata(31, 0)),
-            es_ms.mm_mask === 0xff.U -> io.mm_rdata
+            io.es_ms.mm_mask === 0x1.U  -> Cat(Fill(Mux(io.es_ms.mm_unsigned, 0.U(1.W), io.mm_rdata(7)), 56),  io.mm_rdata(7, 0)),
+            io.es_ms.mm_mask === 0x3.U  -> Cat(Fill(Mux(io.es_ms.mm_unsigned, 0.U(1.W), io.mm_rdata(15)), 48), io.mm_rdata(15, 0)),
+            io.es_ms.mm_mask === 0xf.U  -> Cat(Fill(Mux(io.es_ms.mm_unsigned, 0.U(1.W), io.mm_rdata(31)), 32), io.mm_rdata(31, 0)),
+            io.es_ms.mm_mask === 0xff.U -> io.mm_rdata
         )
     )
 
