a590b017bfe3a2b334f9ff1f222e23606431ea3e
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-17 10:50:15 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:50:15 up  1:04,  0 users,  load average: 0.30, 0.12, 0.03

--- npc/src/src/ES.scala
+++ npc/src/src/ES.scala
@@ -17,15 +17,14 @@ class ES extends Module
     val alu = Module(new Alu)
     io.ds_es.alu_in <> alu.io.in
 
-    io.mm_ren := ds_es.mm_ren
+    io.mm_ren := io.ds_es.mm_ren
+    io.mm_raddr := alu.io.alu_result
+    io.mm_wen := io.ds_es.mm_wen
     io.mm_raddr := alu.alu_result
-    io.mm_wen := ds_es.mm_wen
-    io.mm_raddr := alu.alu_result
-    io.mm_wdata := ds_es.mm_wdata
+    io.mm_wdata := io.ds_es.mm_wdata
 
     io.es_mm.alu_result := alu.io.alu_result
     io.es_mm.rf_wen := io.ds_es.rf_wen
     io.es_mm.rf_waddr := io.ds_es.rf_waddr
-    io.es_mm.mm_waddr := io.ds_es.mm_waddr
     io.es_mm.res_from_mem := io.ds_es.res_from_mem
 }
