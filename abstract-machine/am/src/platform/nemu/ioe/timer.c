073db134621c1ed0c133ff98b422b6574e2cb825
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-20 23:11:09 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:11:09 up  3:15,  0 users,  load average: 0.08, 0.08, 0.03

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -5,8 +5,7 @@ void __am_timer_init() {
 }
 
 void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
-  // uptime->us = io_read(AM_TIMER_UPTIME).us;
-  uptime->us += 1;
+  uptime->us = io_read(AM_TIMER_UPTIME).us;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
