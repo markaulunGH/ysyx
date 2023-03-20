2a49ac692d9f36dd92d0b8debf93f3de6be8dedd
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-20 22:51:25 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:51:25 up  2:55,  0 users,  load average: 0.11, 0.08, 0.06

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -6,14 +6,68 @@
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
 int printf(const char *fmt, ...) {
-  char buf[1024];
+  // char buf[1024];
+  // va_list arg;
+  // va_start(arg, fmt);
+  // sprintf(buf, fmt, arg);
+  // va_end(arg);
+  // // for (int i = 0; buf[i]; ++ i) {
+  // //   putch(buf[i]);
+  // // }
+
+
   va_list arg;
-  va_start(arg, fmt);
-  sprintf(buf, fmt, arg);
+
+  // for (int i = 0; fmt[i]; ++ i)
+  //   putch(fmt[i]);
+
+  va_start (arg, fmt);
+  for (; *fmt; ++ fmt) {
+    switch (*fmt) {
+    case '%':
+      ++ fmt;
+      switch (*fmt)
+      {
+      case 's':
+        char *s = va_arg(arg, char *);
+        while (*s) {
+          putch(*s ++);
+        }
+        break;
+
+      case 'c':
+        putch(va_arg(arg, int));
+        break;
+
+      case 'd':
+        int d = va_arg(arg, int);
+        char tmp[20];
+        int ptr = 0;
+        while (d) {
+          tmp[ptr ++] = d % 10 + '0';
+          d /= 10;
+        }
+        while (ptr) {
+          putch(tmp[-- ptr]);
+        }
+        break;
+      }
+    case '\\':
+      ++ fmt;
+      switch (*fmt)
+      {
+      case 'n':
+        putch('\n');
+        break;
+      }
+    
+    default:
+      putch(*fmt);
+      break;
+    }
+  }
   va_end(arg);
-  // for (int i = 0; buf[i]; ++ i) {
-  //   putch(buf[i]);
-  // }
+
   return 1;
 }
 
