845deacfd15da32550c26ff87dbe74518ce68c76
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-20 23:02:39 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:02:39 up  3:07,  0 users,  load average: 0.06, 0.05, 0.02

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -32,8 +32,6 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
         break;
 
       case 'c':
-        char ch = va_arg(ap, int);
-        putch(ch);
         *out ++ = va_arg(ap, int);
         break;
 
