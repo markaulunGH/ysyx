742c1a4a8919d45875b810ac9c685e4d2ffe47ad
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-21 14:02:10 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:02:10 up  2:27,  0 users,  load average: 0.22, 0.11, 0.03

--- abstract-machine/klib/src/stdlib.c
+++ abstract-machine/klib/src/stdlib.c
@@ -33,9 +33,6 @@ void *malloc(size_t size) {
   // On native, malloc() will be called during initializaion of C runtime.
   // Therefore do not call panic() here, else it will yield a dead recursion:
   //   panic() -> putchar() -> (glibc) -> malloc() -> panic()
-// #if !(defined(__ISA_NATIVE__) && defined(__NATIVE_USE_KLIB__))
-//   panic("Not implemented");
-// #endif
   static void *addr = (void *) -1;
   if (addr == (void *) -1) {
     addr = (void *) ROUNDUP(heap.start, 8);
