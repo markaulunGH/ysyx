408fd07f0fef6118fc7a68b339246eedacde3721
ysyx_22051094-Li Yanjun <liyanjun20@mails.ucas.ac.cn>
2023-03-21 14:20:16 +0800

    complete vga

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -2,19 +2,36 @@
 #include <nemu.h>
 
 #define SYNC_ADDR (VGACTL_ADDR + 4)
+#define SCREEN_W 400
+#define SCREEN_H 300
 
 void __am_gpu_init() {
+  int i;
+  int w = SCREEN_W;
+  int h = SCREEN_H;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  for (i = 0; i < w * h; i ++) {
+    fb[i] = i;
+  }
+  outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
-    .width = 0, .height = 0,
-    .vmemsz = 0
+    .width = SCREEN_W, .height = SCREEN_H,
+    .vmemsz = SCREEN_W * SCREEN_H * sizeof(uint32_t)
   };
 }
 
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *pixels = (uint32_t *) ctl->pixels;
+  for (int i = 0; i < ctl->h; i ++) {
+    for (int j = 0; j < ctl->w; j ++) {
+      fb[(ctl->y + i) * SCREEN_W + ctl->x + j] = pixels[i * ctl->w + j];
+    }
+  }
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
