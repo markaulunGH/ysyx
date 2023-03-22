0f7cb929cb40de2c914728922f33ded33a91616a
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-22 10:33:01 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:33:01 up  2:01,  0 users,  load average: 0.17, 0.52, 0.39

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -22,16 +22,32 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
     switch (*fmt) {
     case '%':
       ++ fmt;
-      switch (*fmt)
-      {
+      int width = 0;
+      char blank = ' ';
+      if (*fmt == '0') {
+        blank = '0';
+        ++ fmt;
+      }
+      while ('0' <= *fmt && *fmt <= '9') {
+        width = width * 10 + *fmt - '0';
+        ++ fmt;
+      }
+      switch (*fmt) {
       case 's':
         char *s = va_arg(ap, char *);
+        int len = strlen(s);
+        for (int i = 0; i < width - len; ++ i) {
+          *out ++ = blank;
+        }
         while (*s) {
           *out ++ = *s ++;
         }
         break;
 
       case 'c':
+        for (int i = 0; i < width - 1; ++ i) {
+          *out ++ = blank;
+        }
         *out ++ = va_arg(ap, int);
         break;
 
@@ -39,9 +55,19 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
         int d = va_arg(ap, int);
         char tmp[20];
         int ptr = 0;
+        if (d < 0) {
+          tmp[ptr ++] = '-';
+          d = -d;
+        }
+        else if (d == 0) {
+          tmp[ptr ++] = '0';
+        }
         while (d) {
           tmp[ptr ++] = d % 10 + '0';
           d /= 10;
+        }
+        for (int i = 0; i < width - ptr; ++ i) {
+          *out ++ = blank;
         }
         while (ptr) {
           *out ++ = tmp[-- ptr];
