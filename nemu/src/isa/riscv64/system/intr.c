c9b505c7ef9d1fa4680bb5ab53d4e2c281577082
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-23 09:19:57 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:19:57 up 37 min,  0 users,  load average: 0.02, 0.01, 0.00

--- nemu/src/isa/riscv64/system/intr.c
+++ nemu/src/isa/riscv64/system/intr.c
@@ -27,7 +27,7 @@ word_t isa_raise_intr(word_t NO, vaddr_t epc) {
 word_t isa_mret() {
   csr_mstatus = (SEXT(BITS(csr_mstatus, 63, 13), 51) << 13) | (BITS(csr_mstatus, 10, 8) << 8) | (1 << 7) |
                 (BITS(csr_mstatus, 6, 4) << 4) | (BITS(csr_mstatus, 7, 7) << 3) | (BITS(csr_mstatus, 2, 0));
-  return csr_mepc;
+  return csr_mepc + 4;
 }
 
 word_t isa_query_intr() {
