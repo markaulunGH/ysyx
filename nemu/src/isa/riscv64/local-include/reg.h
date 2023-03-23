f5ac7eea9e257672e6d2fc3dbd79e0e9b402159c
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-23 08:53:35 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:53:35 up 10 min,  0 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/isa/riscv64/local-include/reg.h
+++ nemu/src/isa/riscv64/local-include/reg.h
@@ -30,4 +30,26 @@ static inline const char* reg_name(int idx, int width) {
   return regs[check_reg_idx(idx)];
 }
 
+static inline int check_csr_idx(int idx) {
+  IFDEF(CONFIG_RT_CHECK, assert(0x341 <= idx && idx <= 0x343));
+  return idx;
+}
+
+static inline int map_csr_idx(int idx) {
+  switch (idx)
+  {
+    case 0x300: return 0;
+    case 0x305: return 1;
+    case 0x341: return 2;
+    case 0x342: return 3;
+    default: return -1;
+  }
+}
+
+#define csr(idx) (cpu.csr[map_csr_idx(idx)])
+#define csr_mstatus csr(0x300)
+#define csr_mtvec csr(0x305)
+#define csr_mepc csr(0x341)
+#define csr_mcause csr(0x342)
+
 #endif
