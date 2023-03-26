1204efb56b3eb97f1cbd3fc1a9ca132944ef165b
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-26 10:30:45 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:30:45 up 2 days,  1:21,  0 users,  load average: 0.18, 0.11, 0.09

--- npc/src/src/Csr.scala
+++ npc/src/src/Csr.scala
@@ -18,10 +18,55 @@ class Csr extends Module
     val mepc = RegInit(0.U(64.W))
     val mcause = RegInit(0.U(64.W))
 
+    val mstatus_uie  = RegInit(0.U(1.W))
+    val mstatus_sie  = RegInit(0.U(1.W))
+    val mstatus_mie  = RegInit(0.U(1.W))
+    val mstatus_upie = RegInit(0.U(1.W))
+    val mstatus_spie = RegInit(0.U(1.W))
+    val mstatus_mpie = RegInit(0.U(1.W))
+    val mstatus_spp  = RegInit(0.U(1.W))
+    val mstatus_mpp  = RegInit(0x3.U(2.W))
+    val mstatus_fs   = RegInit(0.U(2.W))
+    val mstatus_xs   = RegInit(0.U(2.W))
+    val mstatus_mprv = RegInit(0.U(1.W))
+    val mstatus_sum  = RegInit(0.U(1.W))
+    val mstatus_mxr  = RegInit(0.U(1.W))
+    val mstatus_tvm  = RegInit(0.U(1.W))
+    val mstatus_tw   = RegInit(0.U(1.W))
+    val mstatus_tsr  = RegInit(0.U(1.W))
+    val mstatus_uxl  = RegInit(0x2.U(2.W))
+    val mstatus_sxl  = RegInit(0x2.U(2.W))
+    val mstatus_sd   = RegInit(0.U(1.W))
+
+    when (io.addr === 0x300.U)
+    {
+
+        mstatus_uie  := io.wdata(0)
+        mstatus_sie  := io.wdata(1)
+        mstatus_mie  := io.wdata(3)
+        mstatus_upie := io.wdata(4)
+        mstatus_spie := io.wdata(5)
+        mstatus_mpie := io.wdata(7)
+        mstatus_spp  := io.wdata(8)
+        mstatus_mpp  := io.wdata(11, 10)
+        mstatus_fs   := io.wdata(13, 12)
+        mstatus_xs   := io.wdata(15, 14)
+        mstatus_mprv := io.wdata(17)
+        mstatus_sum  := io.wdata(18)
+        mstatus_mxr  := io.wdata(19)
+        mstatus_tvm  := io.wdata(20)
+        mstatus_tw   := io.wdata(21)
+        mstatus_tsr  := io.wdata(22)
+        mstatus_uxl  := io.wdata(25, 24)
+        mstatus_sxl  := io.wdata(27, 26)
+        mstatus_sd   := io.wdata(63)
+    }
+
     when (io.wen && io.addr === 0x300.U)
     {
         mstatus := io.wdata
     }
+
     when (io.wen && io.addr === 0x305.U)
     {
         mtvec := io.wdata
@@ -35,10 +80,10 @@ class Csr extends Module
         mcause := io.wdata
     }
 
-    io.rdata := MuxCase(0.U, Seq(
-        (io.addr === 0x300.U) -> mstatus,
-        (io.addr === 0x305.U) -> mtvec,
-        (io.addr === 0x341.U) -> mepc,
-        (io.addr === 0x342.U) -> mcause
+    io.rdata := MuxLookup(io.addr, 0.U)(Seq(
+        0x300 -> Cat(mstatus_sd, 0.U(27.W), mstatus_sxl, mstatus_uxl, 0.U(9.W), mstatus_tsr, mstatus_tw, mstatus_tvm, mstatus_mxr, mstatus_sum, mstatus_mprv, mstatus_xs, mstatus_fs, mstatus_mpp, 0.U(2.W), mstatus_spp, mstatus_mpie, 0.U(1.W), mstatus_spie, mstatus_upie, mstatus_mie, 0.U(1.W), mstatus_sie, mstatus_uie),
+        0x305 -> mtvec,
+        0x341 -> mepc,
+        0x342 -> mcause
     ))
 }
