d8c09fded9ed40c85de3f65467a0c27ce94d1787
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-26 11:30:28 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:30:28 up 2 days,  2:21,  0 users,  load average: 0.04, 0.11, 0.13

--- nanos-lite/src/irq.c
+++ nanos-lite/src/irq.c
@@ -3,7 +3,7 @@
 static Context* do_event(Event e, Context* c) {
   switch (e.event) {
     case EVENT_YIELD:
-      Log("Yield");
+      Log("fxxk");
       c->mepc += 4;
       break;
     default: panic("Unhandled event ID = %d", e.event);
