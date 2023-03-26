9c8b554ee631b18b21cd7e56fac9183c5b2b0652
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-26 09:21:21 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:21:21 up 2 days, 11 min,  0 users,  load average: 0.77, 0.38, 0.19

--- npc/src/src/WS.scala
+++ npc/src/src/WS.scala
@@ -15,7 +15,7 @@ class WS extends Module
     io.reg_w.waddr := io.ms_ws.rf_waddr
     io.reg_w.wdata := Mux(io.ms_ws.csr_wen, io.csr.rdata, io.ms_ws.rf_wdata)
 
-    io.csr.addr := io.ms_ws.csr_waddr
+    io.csr.addr := io.ms_ws.csr_addr
     io.csr.wen := io.ms_ws.csr_wen
     io.csr.wdata := (io.ms_ws.csr_wdata & io.ms_ws.csr_wmask) | (io.csr.rdata & ~io.ms_ws.csr_wmask)
 }
