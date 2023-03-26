63977fb061647679f99776e07850381ab3638976
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-26 11:26:44 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:26:44 up 2 days,  2:17,  0 users,  load average: 0.16, 0.13, 0.14

--- npc/src/src/Csr.scala
+++ npc/src/src/Csr.scala
@@ -46,71 +46,73 @@ class Csr extends Module
     val mstatus_uxl  = RegInit(0x2.U(2.W))
     val mstatus_sxl  = RegInit(0x2.U(2.W))
     val mstatus_sd   = RegInit(0.U(1.W))
-    when (io.exc)
+    when (io.csr_rw.exc)
     {
         mstatus_mpp := 0x3.U
         mstatus_mpie := mstatus_mie
         mstatus_mie := 0.U
     }
-    .elsewhen (io.mret)
+    .elsewhen (io.csr_rw.mret)
     {
         mstatus_mpp := 0.U
         mstatus_mpie := 0.U
         mstatus_mie := mstatus_mpie
     }
-    .elsewhen (io.wen && io.addr === 0x300.U)
+    .elsewhen (io.csr_rw.wen && io.csr_rw.addr === 0x300.U)
     {
-        mstatus_uie  := io.wdata(0)
-        mstatus_sie  := io.wdata(1)
-        mstatus_mie  := io.wdata(3)
-        mstatus_upie := io.wdata(4)
-        mstatus_spie := io.wdata(5)
-        mstatus_mpie := io.wdata(7)
-        mstatus_spp  := io.wdata(8)
-        mstatus_mpp  := io.wdata(11, 10)
-        mstatus_fs   := io.wdata(13, 12)
-        mstatus_xs   := io.wdata(15, 14)
-        mstatus_mprv := io.wdata(17)
-        mstatus_sum  := io.wdata(18)
-        mstatus_mxr  := io.wdata(19)
-        mstatus_tvm  := io.wdata(20)
-        mstatus_tw   := io.wdata(21)
-        mstatus_tsr  := io.wdata(22)
-        mstatus_uxl  := io.wdata(25, 24)
-        mstatus_sxl  := io.wdata(27, 26)
-        mstatus_sd   := io.wdata(63)
+        mstatus_uie  := io.csr_rw.wdata(0)
+        mstatus_sie  := io.csr_rw.wdata(1)
+        mstatus_mie  := io.csr_rw.wdata(3)
+        mstatus_upie := io.csr_rw.wdata(4)
+        mstatus_spie := io.csr_rw.wdata(5)
+        mstatus_mpie := io.csr_rw.wdata(7)
+        mstatus_spp  := io.csr_rw.wdata(8)
+        mstatus_mpp  := io.csr_rw.wdata(11, 10)
+        mstatus_fs   := io.csr_rw.wdata(13, 12)
+        mstatus_xs   := io.csr_rw.wdata(15, 14)
+        mstatus_mprv := io.csr_rw.wdata(17)
+        mstatus_sum  := io.csr_rw.wdata(18)
+        mstatus_mxr  := io.csr_rw.wdata(19)
+        mstatus_tvm  := io.csr_rw.wdata(20)
+        mstatus_tw   := io.csr_rw.wdata(21)
+        mstatus_tsr  := io.csr_rw.wdata(22)
+        mstatus_uxl  := io.csr_rw.wdata(25, 24)
+        mstatus_sxl  := io.csr_rw.wdata(27, 26)
+        mstatus_sd   := io.csr_rw.wdata(63)
     }
 
     val mtvec = RegInit(0.U(64.W))
-    when (io.wen && io.addr === 0x305.U)
+    when (io.csr_rw.wen && io.csr_rw.addr === 0x305.U)
     {
-        mtvec := io.wdata
+        mtvec := io.csr_rw.wdata
     }
 
     val mepc = RegInit(0.U(64.W))
-    when (io.exc)
+    when (io.csr_rw.exc)
     {
-        mepc := io.pc
+        mepc := io.csr_rw.pc
     }
-    .elsewhen (io.wen && io.addr === 0x341.U)
+    .elsewhen (io.csr_rw.wen && io.csr_rw.addr === 0x341.U)
     {
-        mepc := io.wdata
+        mepc := io.csr_rw.wdata
     }
 
     val mcause = RegInit(0.U(64.W))
-    when (io.exc)
+    when (io.csr_rw.exc)
     {
-        mcause := io.exc_cause
+        mcause := io.csr_rw.exc_cause
     }
-    .elsewhen (io.wen && io.addr === 0x342.U)
+    .elsewhen (io.csr_rw.wen && io.csr_rw.addr === 0x342.U)
     {
-        mcause := io.wdata
+        mcause := io.csr_rw.wdata
     }
 
-    io.rdata := MuxLookup(io.addr, 0.U, Seq(
+    io.csr_rw.rdata := MuxLookup(io.csr_rw.addr, 0.U, Seq(
         0x300.U -> Cat(mstatus_sd, 0.U(27.W), mstatus_sxl, mstatus_uxl, 0.U(9.W), mstatus_tsr, mstatus_tw, mstatus_tvm, mstatus_mxr, mstatus_sum, mstatus_mprv, mstatus_xs, mstatus_fs, mstatus_mpp, 0.U(2.W), mstatus_spp, mstatus_mpie, 0.U(1.W), mstatus_spie, mstatus_upie, mstatus_mie, 0.U(1.W), mstatus_sie, mstatus_uie),
         0x305.U -> mtvec,
         0x341.U -> mepc,
         0x342.U -> mcause
     ))
+    io.csr_pc.mtvec := mtvec
+    io.csr_pc.mepc := mepc
 }
