592b0768d718eb380941ef710612f75a4f50d3e3
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-28 21:25:44 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:25:44 up 11:18,  0 users,  load average: 0.14, 0.06, 0.01

--- npc/src/csrc/cpu/cpu-exec.cpp
+++ npc/src/csrc/cpu/cpu-exec.cpp
@@ -170,7 +170,7 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc)
         puts(_this->logbuf);
 #endif
     }
-#ifdef CONFIG_ETRACE_COND
+#ifdef CONFIG_ETRACE
   if (cpu.pc == cpu.csr[1]) {
     if (ETRACE_COND) {
       log_write("Exception cause %ld at %lx\n", cpu.csr[3], cpu.pc);
