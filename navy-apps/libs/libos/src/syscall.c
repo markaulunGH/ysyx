eff2b39e15e8c0264b7ecbf5b893a78cac759193
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-30 19:57:42 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:57:42 up  4:11,  0 users,  load average: 0.00, 0.00, 0.00

--- navy-apps/libs/libos/src/syscall.c
+++ navy-apps/libs/libos/src/syscall.c
@@ -65,8 +65,18 @@ int _write(int fd, void *buf, size_t count) {
 }
 
 void *_sbrk(intptr_t increment) {
-  extern char end;
-  return (void*) _syscall_(SYS_brk, increment, 0, 0);
+  extern intptr_t end;
+  static intptr_t brk = 0;
+  if (brk == 0) {
+    brk = end;
+  }
+  if (_syscall_(SYS_brk, brk + increment, 0, 0) == 0) {
+    intptr_t old_brk = brk;
+    brk += increment;
+    return (void*) old_brk;
+  } else {
+    return (void*) -1;
+  }
 }
 
 int _read(int fd, void *buf, size_t count) {
