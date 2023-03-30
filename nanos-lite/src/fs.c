8b2260f92b0ce2de2c5dca8ac78fdcf82308b3ce
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-30 20:58:55 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:58:55 up  5:12,  0 users,  load average: 0.04, 0.04, 0.00

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -13,6 +13,13 @@ typedef struct {
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB};
 
+extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
+extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
+extern size_t serial_write(const void *buf, size_t offset, size_t len);
+extern size_t events_read(void *buf, size_t offset, size_t len);
+extern size_t dispinfo_read(void *buf, size_t offset, size_t len);
+extern size_t fb_write(const void *buf, size_t offset, size_t len);
+
 size_t invalid_read(void *buf, size_t offset, size_t len) {
   panic("should not reach here");
   return 0;
@@ -26,8 +33,8 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
-  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, invalid_write},
-  [FD_STDERR] = {"stderr", 0, 0, invalid_read, invalid_write},
+  [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
+  [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
 #include "files.h"
 };
 
@@ -44,23 +51,14 @@ int fs_open(const char *pathname, int flags, int mode) {
   panic("File %s not found\n", pathname);
 }
 
-extern size_t ramdisk_read(void *buf, size_t offset, size_t len);
-extern size_t ramdisk_write(const void *buf, size_t offset, size_t len);
-
 size_t fs_read(int fd, void *buf, size_t len) {
-  size_t read_size = ramdisk_read(buf, file_table[fd].disk_offset, len);
+  size_t read_size = file_table[fd].read ? file_table[fd].read(buf, file_table[fd].disk_offset, len) : ramdisk_read(buf, file_table[fd].disk_offset, len);
   file_table[fd].disk_offset += read_size;
   return read_size;
 }
 
 size_t fs_write(int fd, const void *buf, size_t len) {
-  if (fd == FD_STDOUT || fd == FD_STDERR) {
-    for (int i = 0; i < len; ++ i) {
-      putch(((char *)buf)[i]);
-    }
-    return len;
-  }
-  size_t write_size = ramdisk_write(buf, file_table[fd].disk_offset, len);
+  size_t write_size = file_table[fd].write ? file_table[fd].write(buf, file_table[fd].disk_offset, len) : ramdisk_write(buf, file_table[fd].disk_offset, len);
   file_table[fd].disk_offset += write_size;
   return write_size;
 }
