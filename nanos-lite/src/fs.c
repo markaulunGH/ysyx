0d389cf242b76cef4367fffc72a55ecf943e50cb
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-30 17:03:25 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:03:25 up  1:17,  0 users,  load average: 0.02, 0.01, 0.00

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -55,10 +55,12 @@ size_t fs_read(int fd, void *buf, size_t len) {
 }
 
 size_t fs_write(int fd, const void *buf, size_t len) {
+  Log("fs_write");
   if (fd == FD_STDOUT || fd == FD_STDERR) {
     for (int i = 0; i < len; ++ i) {
       putch(((char *)buf)[i]);
     }
+    Log("complete");
     return len;
   }
   size_t write_size = ramdisk_write(buf, file_table[fd].disk_offset, len);
