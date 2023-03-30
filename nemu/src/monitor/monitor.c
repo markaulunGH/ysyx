4c7f4cab77186baf41fcceb5a647c1fd470dd40a
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-30 16:35:12 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:35:12 up 49 min,  0 users,  load average: 0.19, 0.10, 0.03

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -113,7 +113,7 @@ void init_monitor(int argc, char *argv[]) {
   init_log(log_file);
 
   /* Initialize ftrace */
-  IFDEF(CONFIG_FTRACE, init_ftrace(elf_file));
+  // IFDEF(CONFIG_FTRACE, init_ftrace(elf_file));
 
   /* Initialize memory. */
   init_mem();
