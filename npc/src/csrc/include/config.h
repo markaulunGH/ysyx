eb19fdf377a60026d0b46d024f7f6e3e42086eae
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-30 17:44:38 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:44:38 up  1:58,  0 users,  load average: 0.15, 0.13, 0.08

--- npc/src/csrc/include/config.h
+++ npc/src/csrc/include/config.h
@@ -1,7 +1,8 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-// #define CONFIG_ITRACE
+#define CONFIG_ITRACE
+#define CONFIG_ITRACE_RING
 // #define CONFIG_FTRACE
 // #define CONFIG_MTRACE
 // #define CONFIG_DTRACE
