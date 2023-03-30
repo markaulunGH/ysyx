b1b3de95847872518df01636757d79f16ef7c353
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-30 17:44:05 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:44:05 up  1:57,  0 users,  load average: 0.18, 0.13, 0.07

--- npc/src/csrc/cpu/cpu-exec.cpp
+++ npc/src/csrc/cpu/cpu-exec.cpp
@@ -111,10 +111,10 @@ void init_ftrace(const char *elf_file)
     }
 
     fseek(elf_fp, symshdr.sh_offset, SEEK_SET);
-    symtab = malloc(symshdr.sh_size);
+    symtab = (Elf64_Sym*) malloc(symshdr.sh_size);
     assert(fread(symtab, symshdr.sh_entsize, symshdr.sh_size / symshdr.sh_entsize, elf_fp));
     fseek(elf_fp, strshdr.sh_offset, SEEK_SET);
-    strtab = malloc(strshdr.sh_size);
+    strtab = (char*) malloc(strshdr.sh_size);
     assert(fread(strtab, 1, strshdr.sh_size, elf_fp));
 
     fclose(elf_fp);
