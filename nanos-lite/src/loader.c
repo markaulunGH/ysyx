f16d7a113eb3cac9d171b6ebbe1e85a35569713c
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-30 16:44:25 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:44:25 up 58 min,  0 users,  load average: 0.11, 0.09, 0.03

--- nanos-lite/src/loader.c
+++ nanos-lite/src/loader.c
@@ -10,24 +10,26 @@
 # define Elf_Phdr Elf32_Phdr
 #endif
 
+void ramdisk_read(void *buf, size_t offset, size_t len);
 static uintptr_t loader(PCB *pcb, const char *filename) {
   Elf_Ehdr ehdr;
-  int fd = fs_open(filename, 0, 0);
-  fs_read(fd, &ehdr, sizeof(ehdr));
+  ramdisk_read(&ehdr, 0, sizeof(ehdr));
+  // int fd = fs_open(filename, 0, 0);
+  // fs_read(fd, &ehdr, sizeof(ehdr));
   assert(*(uint32_t *) ehdr.e_ident == *(uint32_t *) "\x7f" "ELF");
 
   for (int i = 0; i < ehdr.e_phnum; i++) {
     Elf_Phdr phdr;
-    fs_lseek(fd, ehdr.e_phoff + i * ehdr.e_phentsize, SEEK_SET);
-    fs_read(fd, &phdr, sizeof(phdr));
+    ramdisk_read(&phdr, ehdr.e_phoff + i * ehdr.e_phentsize, sizeof(phdr));
+    // fs_lseek(fd, ehdr.e_phoff + i * ehdr.e_phentsize, SEEK_SET);
+    // fs_read(fd, &phdr, sizeof(phdr));
     if (phdr.p_memsz != 0 && phdr.p_type == PT_LOAD) {
-      fs_lseek(fd, phdr.p_offset, SEEK_SET);
-      fs_read(fd, (void *)phdr.p_vaddr, phdr.p_filesz);
+      ramdisk_read((void *)phdr.p_vaddr, phdr.p_offset, phdr.p_filesz);
+      // fs_lseek(fd, phdr.p_offset, SEEK_SET);
+      // fs_read(fd, (void *)phdr.p_vaddr, phdr.p_filesz);
       memset((void *)(phdr.p_vaddr + phdr.p_filesz), 0, phdr.p_memsz - phdr.p_filesz);
     }
   }
-
-  Log("fxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxk\n");
 
   return ehdr.e_entry;
 }
