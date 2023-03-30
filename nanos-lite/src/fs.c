d8236b12fc65db08e72991983d130338c0a5d30e
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-30 16:47:06 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:47:06 up  1:00,  0 users,  load average: 0.01, 0.06, 0.02

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -56,7 +56,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
 size_t fs_write(int fd, const void *buf, size_t len) {
   if (fd == FD_STDOUT || fd == FD_STDERR) {
     for (int i = 0; i < len; ++ i) {
-      // putch(((char *)buf)[i]);
+      putch(((char *)buf)[i]);
     }
     return len;
   }
