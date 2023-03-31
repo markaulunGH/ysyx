64b9709ee8cf80c38ed13b449abe458b8d915605
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-31 11:01:11 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:01:11 up  2:08,  0 users,  load average: 0.09, 0.07, 0.02

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -32,10 +32,10 @@ size_t invalid_write(const void *buf, size_t offset, size_t len) {
 
 /* This is the information about all files in disk. */
 static Finfo file_table[] __attribute__((used)) = {
-  [FD_EVENTS] = {"/dev/events", 0, 0, events_read, invalid_write},
   [FD_STDIN]  = {"stdin", 0, 0, invalid_read, invalid_write},
   [FD_STDOUT] = {"stdout", 0, 0, invalid_read, serial_write},
   [FD_STDERR] = {"stderr", 0, 0, invalid_read, serial_write},
+  [FD_EVENTS] = {"/dev/events", 0, 0, events_read, invalid_write},
 #include "files.h"
 };
 
