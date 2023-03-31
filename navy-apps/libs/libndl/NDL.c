85024f8cd0fb721dd9ce9fe68b59c299b3a6ba5f
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-31 11:11:51 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:11:51 up  2:19,  0 users,  load average: 0.43, 0.36, 0.15

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -16,7 +16,7 @@ uint32_t NDL_GetTicks() {
 
 int NDL_PollEvent(char *buf, int len) {
   if (evtdev < 0) {
-    evtdev = open("/dev/event", 0);
+    evtdev = open("/dev/events", 0);
   }
   return read(evtdev, buf, len) > 0;
   return 0;
