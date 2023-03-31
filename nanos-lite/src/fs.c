a5990b626630bef98a64c08681a014913eb07782
tracer-ysyx2205 <tracer@ysyx.org>
2023-03-31 16:27:44 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:27:44 up  5:52,  0 users,  load average: 0.12, 0.07, 0.01

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -42,8 +42,8 @@ static Finfo file_table[] __attribute__((used)) = {
 };
 
 void init_fs() {
-  AM_GPU_CONFIG_T info = io_read(AM_GPU_CONFIG);
-  file_table[FD_FB].size = info.width * info.height * 4;
+  // AM_GPU_CONFIG_T info = io_read(AM_GPU_CONFIG);
+  // file_table[FD_FB].size = info.width * info.height * 4;
 }
 
 int fs_open(const char *pathname, int flags, int mode) {
@@ -70,7 +70,7 @@ size_t fs_write(int fd, const void *buf, size_t len) {
 size_t fs_lseek(int fd, size_t offset, int whence) {
   size_t disk_start = 0;
   for (int i = 0; i < fd; ++ i) {
-    if (file_table[i].disk_offset)
+    // if (file_table[i].disk_offset)
       disk_start += file_table[i].size;
   }
   switch (whence) {
