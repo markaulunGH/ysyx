3ce1bc1105c99ae31c1b2789b8f1286ac7076902
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-01 20:16:14 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:16:14 up  5:11,  0 users,  load average: 0.03, 0.49, 0.87

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -58,7 +58,7 @@ int fs_open(const char *pathname, int flags, int mode) {
 
 size_t fs_read(int fd, void *buf, size_t len) {
   printf("read %s %d\n", file_table[fd].name, file_table[fd].open_offset);
-  size_t read_len = file_table[fd].open_offset + len > file_table[fd].size ? file_table[fd].size - file_table[fd].open_offset : len;
+  size_t read_len = file_table[fd].size && file_table[fd].open_offset + len > file_table[fd].size ? file_table[fd].size - file_table[fd].open_offset : len;
   read_len = file_table[fd].read ? file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, read_len) : ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, read_len);
   file_table[fd].open_offset += read_len;
   return read_len;
@@ -66,7 +66,7 @@ size_t fs_read(int fd, void *buf, size_t len) {
 
 size_t fs_write(int fd, const void *buf, size_t len) {
   printf("write %s %d\n", file_table[fd].name, file_table[fd].open_offset);
-  size_t write_len = file_table[fd].open_offset + len > file_table[fd].size ? file_table[fd].size - file_table[fd].open_offset : len;
+  size_t write_len = file_table[fd].size && file_table[fd].open_offset + len > file_table[fd].size ? file_table[fd].size - file_table[fd].open_offset : len;
   write_len = file_table[fd].write ? file_table[fd].write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, write_len) : ramdisk_write(buf, file_table[fd].disk_offset + file_table[fd].open_offset, write_len);
   file_table[fd].open_offset += write_len;
   return write_len;
