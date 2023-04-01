f23860ba38279a9a41c6c10d9db3ed812b0695c2
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-01 23:18:04 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:18:04 up  1:34,  0 users,  load average: 0.12, 0.58, 0.73

--- npc/src/csrc/sim/sim.cpp
+++ npc/src/csrc/sim/sim.cpp
@@ -2,15 +2,15 @@
 
 const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
 const std::unique_ptr<VTop> top{new VTop{contextp.get(), "TOP"}};
-// VerilatedFstC* tfp = new VerilatedFstC;
+VerilatedFstC* tfp = new VerilatedFstC;
 
 void cycle_end()
 {
-    // tfp->dump(contextp->time());
+    tfp->dump(contextp->time());
     contextp->timeInc(1);
     top->clock = 0;
     top->eval();
-    // tfp->dump(contextp->time());
+    tfp->dump(contextp->time());
     contextp->timeInc(1);
     top->clock = 1;
     top->eval();
@@ -29,13 +29,13 @@ void init_simulation(int argc, char** argv)
 {
     Verilated::mkdir("logs");
 
-    // contextp->debug(0);
+    contextp->debug(0);
     contextp->randReset(2);
-    // contextp->traceEverOn(true);
-    // contextp->commandArgs(argc, argv);
+    contextp->traceEverOn(true);
+    contextp->commandArgs(argc, argv);
 
-    // top->trace(tfp, 0);
-    // tfp->open("logs/dump.fst");
+    top->trace(tfp, 0);
+    tfp->open("logs/dump.fst");
 
     reset();
 }
