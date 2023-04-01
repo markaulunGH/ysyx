b4f88dd2407a6825346888409d187780bc6c492e
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-01 17:59:59 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:59:59 up  2:55,  0 users,  load average: 1.80, 1.69, 1.15

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -51,7 +51,7 @@ int fs_open(const char *pathname, int flags, int mode) {
   for (int i = 0; i < sizeof(file_table) / sizeof(Finfo); ++ i) {
     if (strcmp(pathname, file_table[i].name) == 0) {
       file_table[i].disk_offset = disk_start;
-      printf("%d\n", disk_start);
+      // printf("%d\n", disk_start);
       return i;
     }
     if (file_table[i].read == NULL || file_table[i].write == NULL) {
