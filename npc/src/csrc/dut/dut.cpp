78696a0c011a315991b07279e4d2aaaa4daeecb0
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-01 22:45:43 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:45:43 up  1:02,  0 users,  load average: 0.01, 0.12, 0.15

--- npc/src/csrc/dut/dut.cpp
+++ npc/src/csrc/dut/dut.cpp
@@ -53,7 +53,7 @@ void checkregs(CPU_state *ref, vaddr_t pc)
     bool result = true;
     if (ref->pc != cpu.pc)
     {
-        printf("Difftest failed at pc = %016lx\n", cpu.pc);
+        printf("Difftest failed\n");
         printf("cpu.pc = %016lx\nref.pc = %016lx", cpu.pc, ref->pc);
         result = false;
     }
@@ -61,7 +61,7 @@ void checkregs(CPU_state *ref, vaddr_t pc)
     {
         if (ref->gpr[i] != cpu.gpr[i])
         {
-            printf("Difftest failed at pc = %016lx %016lx\n", cpu.pc, ref->pc);
+            printf("Difftest failed\n");
             printf("cpu.gpr[%d] = %016lx\nref.gpr[%d] = %016lx\n", i, cpu.gpr[i], i, ref->gpr[i]);
             result = false;
         }
