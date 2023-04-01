9de192a56739ae860d9dba82cf9049cd9e005c18
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-01 21:20:12 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:20:12 up  6:15,  0 users,  load average: 0.22, 0.10, 0.10

--- navy-apps/libs/libminiSDL/src/event.c
+++ navy-apps/libs/libminiSDL/src/event.c
@@ -46,6 +46,6 @@ int SDL_PeepEvents(SDL_Event *ev, int numevents, int action, uint32_t mask) {
 }
 
 uint8_t* SDL_GetKeyState(int *numkeys) {
-  *numkeys = sizeof(keystate);
+  *numkeys = sizeof(keyname);
   return keystate;
 }
