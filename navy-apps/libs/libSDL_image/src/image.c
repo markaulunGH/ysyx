f7b80243886bec133d19cbb8f15a8f7842585675
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-01 17:06:08 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:06:08 up  2:01,  0 users,  load average: 0.12, 0.04, 0.01

--- navy-apps/libs/libSDL_image/src/image.c
+++ navy-apps/libs/libSDL_image/src/image.c
@@ -12,7 +12,15 @@ SDL_Surface* IMG_Load_RW(SDL_RWops *src, int freesrc) {
 }
 
 SDL_Surface* IMG_Load(const char *filename) {
-  return NULL;
+  FILE *fp = fopen(filename, "r");
+  fseek(fp, 0, SEEK_END);
+  size_t size = ftell(fp);
+  char *buf = malloc(size);
+  fseek(fp, 0, SEEK_SET);
+  fread(buf, 1, size, fp);
+  fclose(fp);
+  SDL_Surface *surface = STBIMG_LoadFromMemory(buf, size);
+  return surface;
 }
 
 int IMG_isPNG(SDL_RWops *src) {
