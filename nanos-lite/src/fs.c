9b9fdb85212d09a2b55948fc4d253e349f737564
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-01 20:07:15 +0800

    > compile NEMU ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:07:15 up  5:03,  0 users,  load average: 1.19, 0.90, 1.09

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -57,6 +57,7 @@ int fs_open(const char *pathname, int flags, int mode) {
 }
 
 size_t fs_read(int fd, void *buf, size_t len) {
+  printf("%s %d\n", file_table[fd].name, file_table[fd].open_offset);
   size_t read_len = file_table[fd].open_offset + len > file_table[fd].size ? file_table[fd].size - file_table[fd].open_offset : len;
   read_len = file_table[fd].read ? file_table[fd].read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, read_len) : ramdisk_read(buf, file_table[fd].disk_offset + file_table[fd].open_offset, read_len);
   file_table[fd].open_offset += read_len;
