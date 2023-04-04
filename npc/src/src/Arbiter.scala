cf1ce89dc740a1091ef7866a7a7d9d5968b82471
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-04 22:49:40 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:49:40 up 11:46,  0 users,  load average: 0.23, 0.12, 0.03

--- npc/src/src/Arbiter.scala
+++ npc/src/src/Arbiter.scala
@@ -34,29 +34,29 @@ class Arbiter extends Module
         widle := true.B
     }
 
-    io.master.aw.valid     := io.data_master.aw.valid
-    io.data_master.aw.ready  := io.master.aw.ready
-    io.master.aw.bits.addr := io.data_master.aw.bits.addr
-    io.master.aw.bits.prot := io.data_master.aw.bits.prot
+    io.master.aw.valid      := io.data_master.aw.valid
+    io.data_master.aw.ready := io.master.aw.ready
+    io.master.aw.bits.addr  := io.data_master.aw.bits.addr
+    io.master.aw.bits.prot  := io.data_master.aw.bits.prot
 
-    io.master.w.valid     := io.data_master.w.valid
-    io.data_master.w.ready  := io.master.w.ready
-    io.master.w.bits.data := io.data_master.w.bits.data
-    io.master.w.bits.strb := io.data_master.w.bits.strb
+    io.master.w.valid      := io.data_master.w.valid
+    io.data_master.w.ready := io.master.w.ready
+    io.master.w.bits.data  := io.data_master.w.bits.data
+    io.master.w.bits.strb  := io.data_master.w.bits.strb
 
     io.data_slave.b.valid     := io.slave.b.valid
-    io.slave.b.ready        := io.data_slave.b.ready
+    io.slave.b.ready          := io.data_slave.b.ready
     io.data_slave.b.bits.resp := io.slave.b.bits.resp
 
-    io.master.ar.valid     := Mux(data_req, io.ms.ar.valid, io.pf.ar.valid) && (widle || io.axi.ar.bits.addr =/= io.axi.aw.bits.addr)
-    io.data_master.ar.ready  := Mux(data_req, io.master.ar.ready, false.B)
-    io.inst_master.ar.ready  := Mux(data_req, false.B, io.master.ar.ready)
-    io.master.ar.bits.addr := Mux(data_req, io.ms.ar.bits.addr, io.pf.ar.bits.addr)
-    io.master.ar.bits.prot := Mux(data_req, io.ms.ar.bits.prot, io.pf.ar.bits.prot)
+    io.master.ar.valid      := Mux(data_req, io.ms.ar.valid, io.pf.ar.valid) && (widle || io.axi.ar.bits.addr =/= io.axi.aw.bits.addr)
+    io.data_master.ar.ready := Mux(data_req, io.master.ar.ready, false.B)
+    io.inst_master.ar.ready := Mux(data_req, false.B, io.master.ar.ready)
+    io.master.ar.bits.addr  := Mux(data_req, io.ms.ar.bits.addr, io.pf.ar.bits.addr)
+    io.master.ar.bits.prot  := Mux(data_req, io.ms.ar.bits.prot, io.pf.ar.bits.prot)
 
     io.data_slave.r.valid     := Mux(data_req, io.slave.r.valid, false.B)
     io.inst_slave.r.valid     := Mux(data_req, false.B, io.slave.r.valid)
-    io.slave.r.ready        := Mux(data_req, io.data_slave.r.ready, io.inst_slave.r.ready)
+    io.slave.r.ready          := Mux(data_req, io.data_slave.r.ready, io.inst_slave.r.ready)
     io.data_slave.r.bits.data := io.slave.r.bits.data
     io.inst_slave.r.bits.data := io.slave.r.bits.data
     io.data_slave.r.bits.resp := io.slave.r.bits.resp
