7408f908db5301e69c5618dd4057578553fe5014
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-04 10:58:21 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:58:21 up 56 min,  0 users,  load average: 0.25, 0.16, 0.06

--- npc/src/src/AXI.scala
+++ npc/src/src/AXI.scala
@@ -3,51 +3,30 @@ import chisel3.util._
 
 class AXI extends Module
 {
-    val io = IO(new Bundle
-    {
-        val axi = new AXI_Lite
-    })
+    val io = IO(new AXI_Lite)
 
     val arinit :: araddr :: Nil = Enum(2)
     val arstate = RegInit(arinit)
     switch (arstate)
     {
-        is (arinit) { arstate := Mux(io.axi.ar.valid, araddr, arinit) }
-        is (araddr)
-        {
-            arstate := Mux(io.axi.ar.ready, arinit, araddr)
-        }
+        is (arinit) { arstate := Mux(io.ar.valid, araddr, arinit) }
+        is (araddr) { arstate := Mux(io.ar.ready, arinit, araddr) }
     }
 
     val rinit :: rdata :: Nil = Enum(2)
     val rstate = RegInit(rinit)
     switch (rstate)
     {
-        is (rinit)
-        {
-            rstate := Mux(io.axi.r.valid, rdata, rinit)
-        }
-        is (rdata)
-        {
-            rstate := rinit
-        }
+        is (rinit) { rstate := Mux(io.r.valid, rdata, rinit) }
+        is (rdata) { rstate := rinit }
     }
 
     val winit :: waddr :: wdata :: Nil = Enum(3)
     val wstate = RegInit(winit)
     switch (wstate)
     {
-        is (winit)
-        {
-            wstate := Mux(io.axi.aw.valid, waddr, winit)
-        }
-        is (waddr)
-        {
-            wstate := Mux(io.axi.aw.ready, wdata, waddr)
-        }
-        is (wdata)
-        {
-            wstate := Mux(io.axi.w.ready, winit, wdata)
-        }
+        is (winit) { wstate := Mux(io.aw.valid, waddr, winit) }
+        is (waddr) { wstate := Mux(io.aw.ready, wdata, waddr) }
+        is (wdata) { wstate := Mux(io.w.ready, winit, wdata) }
     }
 }
