10d747ce4efc0d8092d17e7838fe4117f20617ec
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-04 12:10:11 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  12:10:11 up  2:08,  0 users,  load average: 0.52, 0.11, 0.03

--- npc/src/src/Arbiter.scala
+++ npc/src/src/Arbiter.scala
@@ -12,13 +12,13 @@ class Arbiter extends Module
 
     val ms_req = io.ms.ar.valid || io.ms.aw.valid
 
-    io.axi.Bits.aw.addr := Mux(ms_req, io.ms.Bits.aw.addr, io.fs.Bits.aw.addr)
-    io.axi.Bits.aw.prot := Mux(ms_req, io.ms.Bits.aw.prot, io.fs.Bits.aw.prot)
-    io.axi.Bits.w.data  := Mux(ms_req, io.ms.Bits.w.data,  io.fs.Bits.w.data)
-    io.axi.Bits.w.strb  := Mux(ms_req, io.ms.Bits.w.strb,  io.fs.Bits.w.strb)
-    io.axi.Bits.b.resp  := Mux(ms_req, io.ms.Bits.b.resp,  io.fs.Bits.b.resp)
-    io.axi.Bits.ar.addr := Mux(ms_req, io.ms.Bits.ar.addr, io.fs.Bits.ar.addr)
-    io.axi.Bits.ar.prot := Mux(ms_req, io.ms.Bits.ar.prot, io.fs.Bits.ar.prot)
-    io.axi.Bits.r.data  := Mux(ms_req, io.ms.Bits.r.data,  io.fs.Bits.r.data)
-    io.axi.Bits.r.resp  := Mux(ms_req, io.ms.Bits.r.resp,  io.fs.Bits.r.resp)
+    io.axi.aw.bits.addr := Mux(ms_req, io.ms.aw.bits.addr, io.fs.aw.bits.addr)
+    io.axi.aw.bits.prot := Mux(ms_req, io.ms.aw.bits.prot, io.fs.aw.bits.prot)
+    io.axi.w.bits.data  := Mux(ms_req, io.ms.w.bits.data,  io.fs.w.bits.data)
+    io.axi.w.bits.strb  := Mux(ms_req, io.ms.w.bits.strb,  io.fs.w.bits.strb)
+    io.axi.b.bits.resp  := Mux(ms_req, io.ms.b.bits.resp,  io.fs.b.bits.resp)
+    io.axi.ar.bits.addr := Mux(ms_req, io.ms.ar.bits.addr, io.fs.ar.bits.addr)
+    io.axi.ar.bits.prot := Mux(ms_req, io.ms.ar.bits.prot, io.fs.ar.bits.prot)
+    io.axi.r.bits.data  := Mux(ms_req, io.ms.r.bits.data,  io.fs.r.bits.data)
+    io.axi.r.bits.resp  := Mux(ms_req, io.ms.r.bits.resp,  io.fs.r.bits.resp)
 }
