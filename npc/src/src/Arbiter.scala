c8c7cbcf6445b06bd162c16da8b25504c393fb68
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-04 19:48:08 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:48:08 up  8:45,  0 users,  load average: 0.10, 0.08, 0.03

--- npc/src/src/Arbiter.scala
+++ npc/src/src/Arbiter.scala
@@ -3,22 +3,22 @@ import chisel3.util._
 
 class Arbiter extends Module
 {
-    val io = IO(new Bundle
-    {
-        val fs = new AXI_Lite_IO
-        val ms = new AXI_Lite_IO
-        val axi = new AXI_Lite_IO
-    })
+    // val io = IO(new Bundle
+    // {
+    //     val fs = new AXI_Lite_IO
+    //     val ms = new AXI_Lite_IO
+    //     val axi = new AXI_Lite_IO
+    // })
 
-    val ms_req = io.ms.ar.valid || io.ms.aw.valid
+    // val ms_req = io.ms.ar.valid || io.ms.aw.valid
 
-    io.axi.aw.bits.addr := Mux(ms_req, io.ms.aw.bits.addr, io.fs.aw.bits.addr)
-    io.axi.aw.bits.prot := Mux(ms_req, io.ms.aw.bits.prot, io.fs.aw.bits.prot)
-    io.axi.w.bits.data  := Mux(ms_req, io.ms.w.bits.data,  io.fs.w.bits.data)
-    io.axi.w.bits.strb  := Mux(ms_req, io.ms.w.bits.strb,  io.fs.w.bits.strb)
-    io.axi.b.bits.resp  := Mux(ms_req, io.ms.b.bits.resp,  io.fs.b.bits.resp)
-    io.axi.ar.bits.addr := Mux(ms_req, io.ms.ar.bits.addr, io.fs.ar.bits.addr)
-    io.axi.ar.bits.prot := Mux(ms_req, io.ms.ar.bits.prot, io.fs.ar.bits.prot)
-    io.axi.r.bits.data  := Mux(ms_req, io.ms.r.bits.data,  io.fs.r.bits.data)
-    io.axi.r.bits.resp  := Mux(ms_req, io.ms.r.bits.resp,  io.fs.r.bits.resp)
+    // io.axi.aw.bits.addr := Mux(ms_req, io.ms.aw.bits.addr, io.fs.aw.bits.addr)
+    // io.axi.aw.bits.prot := Mux(ms_req, io.ms.aw.bits.prot, io.fs.aw.bits.prot)
+    // io.axi.w.bits.data  := Mux(ms_req, io.ms.w.bits.data,  io.fs.w.bits.data)
+    // io.axi.w.bits.strb  := Mux(ms_req, io.ms.w.bits.strb,  io.fs.w.bits.strb)
+    // io.axi.b.bits.resp  := Mux(ms_req, io.ms.b.bits.resp,  io.fs.b.bits.resp)
+    // io.axi.ar.bits.addr := Mux(ms_req, io.ms.ar.bits.addr, io.fs.ar.bits.addr)
+    // io.axi.ar.bits.prot := Mux(ms_req, io.ms.ar.bits.prot, io.fs.ar.bits.prot)
+    // io.axi.r.bits.data  := Mux(ms_req, io.ms.r.bits.data,  io.fs.r.bits.data)
+    // io.axi.r.bits.resp  := Mux(ms_req, io.ms.r.bits.resp,  io.fs.r.bits.resp)
 }
--- npc/src/src/PF.scala
+++ npc/src/src/PF.scala
@@ -3,27 +3,27 @@ import chisel3.util._
 
 class PF extends Module
 {
-    val io = IO(new Bundle
-    {
-        val pf_fs = new PF_FS
+    // val io = IO(new Bundle
+    // {
+    //     val pf_fs = new PF_FS
 
-        val inst_axi = new AXI_Lite_Master
-    })
+    //     val inst_axi = new AXI_Lite_Master
+    // })
 
-    val pc = RegInit(0x7ffffffc.U(64.W))
-    val next_pc = Mux(io.pf_fs.br_taken, io.pf_fs.br_target, pc + 4.U)
+    // val pc = RegInit(0x7ffffffc.U(64.W))
+    // val next_pc = Mux(io.pf_fs.br_taken, io.pf_fs.br_target, pc + 4.U)
 
-    io.inst_axi.ar.valid := true.B
-    io.inst_axi.ar.bits.addr := next_pc
-    io.inst_axi.ar.bits.prot := 0.U(3.W)
-    when (io.inst_axi.ar.fire)
-    {
-        pc := next_pc
-    }
-    io.inst_axi.aw.valid := false.B
-    io.inst_axi.aw.bits.addr := 0.U(64.W)
-    io.inst_axi.aw.bits.prot := 0.U(3.W)
-    io.inst_axi.w.valid := false.B
-    io.inst_axi.w.bits.data := 0.U(64.W)
-    io.inst_axi.w.bits.strb := 0.U(8.W)
+    // io.inst_axi.ar.valid := true.B
+    // io.inst_axi.ar.bits.addr := next_pc
+    // io.inst_axi.ar.bits.prot := 0.U(3.W)
+    // when (io.inst_axi.ar.fire)
+    // {
+    //     pc := next_pc
+    // }
+    // io.inst_axi.aw.valid := false.B
+    // io.inst_axi.aw.bits.addr := 0.U(64.W)
+    // io.inst_axi.aw.bits.prot := 0.U(3.W)
+    // io.inst_axi.w.valid := false.B
+    // io.inst_axi.w.bits.data := 0.U(64.W)
+    // io.inst_axi.w.bits.strb := 0.U(8.W)
 }
