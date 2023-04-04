545ba0de1600410ce947305abf50077f2a08daea
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-04 11:56:30 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:56:30 up  1:54,  0 users,  load average: 0.28, 0.10, 0.03

--- npc/src/src/Arbiter.scala
+++ npc/src/src/Arbiter.scala
@@ -12,13 +12,13 @@ class Arbiter extends Module
 
     val ms_req = io.ms.ar.valid || io.ms.aw.valid
 
-    // io.axi.aw.addr := Mux(ms_req, io.ms.aw.addr, io.fs.aw.addr)
-    // io.axi.aw.prot := Mux(ms_req, io.ms.aw.prot, io.fs.aw.prot)
-    // io.axi.w.data  := Mux(ms_req, io.ms.w.data,  io.fs.w.data)
-    // io.axi.w.strb  := Mux(ms_req, io.ms.w.strb,  io.fs.w.strb)
-    // io.axi.b.resp  := Mux(ms_req, io.ms.b.resp,  io.fs.b.resp)
-    // io.axi.ar.addr := Mux(ms_req, io.ms.ar.addr, io.fs.ar.addr)
-    // io.axi.ar.prot := Mux(ms_req, io.ms.ar.prot, io.fs.ar.prot)
-    // io.axi.r.data  := Mux(ms_req, io.ms.r.data,  io.fs.r.data)
+    io.axi.aw.addr := Mux(ms_req, io.ms.aw.addr, io.fs.aw.addr)
+    io.axi.aw.prot := Mux(ms_req, io.ms.aw.prot, io.fs.aw.prot)
+    io.axi.w.data  := Mux(ms_req, io.ms.w.data,  io.fs.w.data)
+    io.axi.w.strb  := Mux(ms_req, io.ms.w.strb,  io.fs.w.strb)
+    io.axi.b.resp  := Mux(ms_req, io.ms.b.resp,  io.fs.b.resp)
+    io.axi.ar.addr := Mux(ms_req, io.ms.ar.addr, io.fs.ar.addr)
+    io.axi.ar.prot := Mux(ms_req, io.ms.ar.prot, io.fs.ar.prot)
+    io.axi.r.data  := Mux(ms_req, io.ms.r.data,  io.fs.r.data)
     io.axi.r.resp  := Mux(ms_req, io.ms.r.resp,  io.fs.r.resp)
 }
