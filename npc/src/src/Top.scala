d6eace0f00fbced559c00c61bbecc312fbafac32
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-05 11:25:00 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:25:00 up  2:48,  0 users,  load average: 0.29, 0.17, 0.07

--- npc/src/src/Top.scala
+++ npc/src/src/Top.scala
@@ -146,8 +146,8 @@ class Top extends Module
     ds.io.ds_es <> es.io.ds_es
     es.io.es_ms <> ms.io.es_ms
     ms.io.ms_ws <> ws.io.ms_ws
-    val reset = Wire(Reset())
-    val ready = fs.io.fs_ready && ds.io.ds_ready && es.io.es_ready && ms.io.ms_ready && ws.io.ws_ready && !reset.asBool()
+    val rst = reset.asBool()
+    val ready = fs.io.fs_ready && ds.io.ds_ready && es.io.es_ready && ms.io.ms_ready && ws.io.ws_ready && !rst.asBool()
     pf.io.ready := ready
     fs.io.ready := ready
     ds.io.ready := ready
