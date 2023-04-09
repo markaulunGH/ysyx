4997e07ac78d098e7d960b288a5e4e7d6c70fb15
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-10 01:31:12 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:31:12 up 16 min,  0 users,  load average: 0.38, 0.26, 0.13

--- npc/src/src/Top.scala
+++ npc/src/src/Top.scala
@@ -191,7 +191,6 @@ class Top extends Module
         val ebreak = Output(Bool())
         val rf = Output(Vec(32, UInt(64.W)))
         val rf_wen = Output(Bool())
-        val ready = Output(Bool())
     })
     
     val pf = Module(new PF)
@@ -237,5 +236,4 @@ class Top extends Module
     io.ebreak := ds.io.ebreak
     io.rf := rf.io.rf
     io.rf_wen := rf.io.rf_wen
-    io.ready := ready
 }
