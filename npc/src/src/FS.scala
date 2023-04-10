2ff73fceaba5f9931ea766e8e80c7c7fbf98d2f6
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-10 19:12:09 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:12:09 up  8:18,  0 users,  load average: 0.14, 0.19, 0.09

--- npc/src/src/FS.scala
+++ npc/src/src/FS.scala
@@ -18,6 +18,7 @@ class FS extends Module
     })
 
     val fs_valid = RegInit(false.B)
+    val rfire = RegInit(true.B) // pretend that there is an instruction in the buffer before the first instruction is fetched; maybe problematic
     val fs_ready = fs_valid && (io.inst_slave.r.fire || rfire)
     val fs_allow_in = !fs_valid || fs_ready && io.ds_fs.ds_allow_in
     val to_ds_valid = fs_valid && fs_ready
@@ -37,7 +38,6 @@ class FS extends Module
     }
 
     val rdata = RegInit(0.U(32.W))
-    val rfire = RegInit(true.B) // pretend that there is an instruction in the buffer before the first instruction is fetched; maybe problematic
 
     io.inst_slave.r.ready := !rfire && !reset.asBool()
     when (fs_allow_in)
