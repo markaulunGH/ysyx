8c045bc0250c4ae3a09d239cfb6f366eac51cba9
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-10 19:26:36 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:26:36 up  8:33,  0 users,  load average: 0.17, 0.23, 0.18

--- npc/src/src/DS.scala
+++ npc/src/src/DS.scala
@@ -192,12 +192,12 @@ class DS extends Module
     read_rf2 := inst_R || inst_S || inst_B
 
     val rs1_value = MuxCase(io.reg_r.rdata1, Seq(
-        (io.reg_r.raddr1 =/= 0.U && io.es_ds.to_mm_valid && io.es_ds.rf_wen && io.reg_r.raddr1 === io.es_ds.rf_waddr) -> io.es_ds.alu_result,
+        (io.reg_r.raddr1 =/= 0.U && io.es_ds.to_ms_valid && io.es_ds.rf_wen && io.reg_r.raddr1 === io.es_ds.rf_waddr) -> io.es_ds.alu_result,
         (io.reg_r.raddr1 =/= 0.U && io.ms_ds.to_ws_valid && io.ms_ds.rf_wen && io.reg_r.raddr1 === io.ms_ds.rf_waddr) -> io.ms_ds.rf_wdata,
         (io.reg_r.raddr1 =/= 0.U && io.ws_ds.valid       && io.ws_ds.rf_wen && io.reg_r.raddr1 === io.ws_ds.rf_waddr) -> io.ws_ds.rf_wdata
     ))
     val rs2_value = MuxCase(io.reg_r.rdata2, Seq(
-        (io.reg_r.raddr2 =/= 0.U && io.es_ds.to_mm_valid && io.es_ds.rf_wen && io.reg_r.raddr2 === io.es_ds.rf_waddr) -> io.es_ds.alu_result,
+        (io.reg_r.raddr2 =/= 0.U && io.es_ds.to_ms_valid && io.es_ds.rf_wen && io.reg_r.raddr2 === io.es_ds.rf_waddr) -> io.es_ds.alu_result,
         (io.reg_r.raddr2 =/= 0.U && io.ms_ds.to_ws_valid && io.ms_ds.rf_wen && io.reg_r.raddr2 === io.ms_ds.rf_waddr) -> io.ms_ds.rf_wdata,
         (io.reg_r.raddr2 =/= 0.U && io.ws_ds.valid       && io.ws_ds.rf_wen && io.reg_r.raddr2 === io.ws_ds.rf_waddr) -> io.ws_ds.rf_wdata
     ))
