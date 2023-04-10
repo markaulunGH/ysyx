5a7007f58d36d5739b38ad93bbe11f2f9ab90f50
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-10 19:26:19 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:26:19 up  8:32,  0 users,  load average: 0.07, 0.21, 0.17

--- npc/src/src/Top.scala
+++ npc/src/src/Top.scala
@@ -105,7 +105,7 @@ class ES_DS extends Bundle
 {
     val es_allow_in = Output(Bool())
     val es_valid = Output(Bool())
-    val to_mm_valid = Output(Bool())
+    val to_ms_valid = Output(Bool())
     val alu_result = Output(UInt(64.W))
     val rf_waddr = Output(UInt(5.W))
     val rf_wen = Output(Bool())
