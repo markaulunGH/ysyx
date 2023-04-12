ade0f43d1907d94ed762edac223e904e3d536429
ysyx_22051094-Li Yanjun <liyanjun20@mails.ucas.ac.cn>
2023-04-12 23:21:44 +0800

    complete pipeline and pass all cpu tests

--- npc/src/src/AXI_Arbiter.scala
+++ npc/src/src/AXI_Arbiter.scala
@@ -14,9 +14,11 @@ class AXI_Arbiter extends Module
     })
 
     val widle = RegInit(true.B)
+    val awaddr = RegInit(0.U(64.W))
     when (io.master.aw.valid)
     {
         widle := false.B
+        awaddr := io.master.aw.bits.addr
     }
     .elsewhen (io.slave.b.fire)
     {
@@ -24,23 +26,23 @@ class AXI_Arbiter extends Module
     }
     
     val ridle = RegInit(true.B)
-    when (io.slave.r.fire)
-    {
-        ridle := true.B
-    }
-    .elsewhen (io.master.ar.valid)
+    when (io.master.ar.valid)
     {
         ridle := false.B
     }
+    .elsewhen (io.slave.r.fire)
+    {
+        ridle := true.B
+    }
 
     val data_req = RegInit(false.B)
-    when (io.data_master.ar.valid && (ridle || io.slave.r.fire))
-    {
-        data_req := true.B
-    }
-    .elsewhen (io.data_slave.r.fire)
+    when (io.data_slave.r.fire)
     {
         data_req := false.B
+    }
+    .elsewhen (io.data_master.ar.valid && (ridle || io.slave.r.fire))
+    {
+        data_req := true.B
     }
 
     io.master.aw.valid      := io.data_master.aw.valid
@@ -61,14 +63,14 @@ class AXI_Arbiter extends Module
     io.data_slave.b.bits.resp := io.slave.b.bits.resp
     io.inst_slave.b.bits.resp := 0.U(3.W)
 
-    //potential bugs down here; maybe fixed when changing to pipeline
-    io.master.ar.valid      := Mux(io.data_master.ar.valid || data_req, io.data_master.ar.valid, io.inst_master.ar.valid) && (widle || io.master.ar.bits.addr =/= io.master.aw.bits.addr) && (ridle || io.slave.r.fire)
-    io.data_master.ar.ready := Mux(io.data_master.ar.valid || data_req, io.master.ar.ready, false.B) && (ridle || io.slave.r.fire)
-    io.inst_master.ar.ready := Mux(io.data_master.ar.valid || data_req, false.B, io.master.ar.ready) && (ridle || io.slave.r.fire)
-    io.master.ar.bits.addr  := Mux(io.data_master.ar.valid || data_req, io.data_master.ar.bits.addr, io.inst_master.ar.bits.addr)
-    io.master.ar.bits.prot  := Mux(io.data_master.ar.valid || data_req, io.data_master.ar.bits.prot, io.inst_master.ar.bits.prot)
+    io.master.ar.valid      := Mux(io.data_master.ar.valid, io.data_master.ar.valid, io.inst_master.ar.valid) && (widle || io.master.ar.bits.addr =/= awaddr) && (ridle || io.slave.r.fire)
+    io.data_master.ar.ready := Mux(io.data_master.ar.valid, io.master.ar.ready, false.B) && (widle || io.data_master.ar.bits.addr =/= awaddr) && (ridle || io.slave.r.fire)
+    // combinational cycle?
+    // io.data_master.ar.ready := Mux(io.data_master.ar.valid, io.master.ar.ready, false.B) && (widle || io.master.ar.bits.addr =/= awaddr) && (ridle || io.slave.r.fire)
+    io.inst_master.ar.ready := Mux(io.data_master.ar.valid, false.B, io.master.ar.ready) && (widle || io.inst_master.ar.bits.addr =/= awaddr) && (ridle || io.slave.r.fire)
+    io.master.ar.bits.addr  := Mux(io.data_master.ar.valid, io.data_master.ar.bits.addr, io.inst_master.ar.bits.addr)
+    io.master.ar.bits.prot  := Mux(io.data_master.ar.valid, io.data_master.ar.bits.prot, io.inst_master.ar.bits.prot)
 
-    //what if instruction fetch is invalid and slave is not fired yet? slave may never receive ready signal
     io.data_slave.r.valid     := Mux(data_req, io.slave.r.valid, false.B)
     io.inst_slave.r.valid     := Mux(data_req, false.B, io.slave.r.valid)
     io.slave.r.ready          := Mux(data_req, io.data_slave.r.ready, io.inst_slave.r.ready)
