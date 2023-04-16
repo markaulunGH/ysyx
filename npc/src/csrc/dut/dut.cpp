7dae5981af9731d00d3cbb559199b5e1a97f1a60
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-16 09:33:40 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:33:40 up 45 min,  0 users,  load average: 0.05, 0.03, 0.00

--- npc/src/csrc/dut/dut.cpp
+++ npc/src/csrc/dut/dut.cpp
@@ -99,7 +99,7 @@ void difftest_step(vaddr_t pc, vaddr_t next_pc)
     {
         printf("skip\n");
         ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
-        printf("copy: %016lx\n", cpu.pc);
+        printf("copy: %x\n", cpu.pc);
         is_skip_ref = false;
         return;
     }
