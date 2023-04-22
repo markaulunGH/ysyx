80652b79354f27cecc10d1f16a9ac37cb093c906
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-22 19:43:16 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:43:16 up 11:11,  0 users,  load average: 1.81, 0.72, 0.38

--- npc/src/src/Top.scala
+++ npc/src/src/Top.scala
@@ -93,8 +93,8 @@ class Top extends Module
     arbiter.io.data_slave  <> ms.data_slave
 
     val sram = Module(new SRAM)
-    sram.io.master <> arbiter.io.master
-    sram.io.slave  <> arbiter.io.slave
+    sram.master <> arbiter.io.master
+    sram.slave  <> arbiter.io.slave
     io.mm_ren := sram.sim.mm_ren
     io.mm_raddr := sram.sim.mm_raddr
     sram.sim.mm_rdata := io.mm_rdata
