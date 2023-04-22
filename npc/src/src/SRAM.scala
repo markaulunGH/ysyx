ed15154d56c6ab562ad7ec8f397440649f0a81fa
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-22 19:42:26 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:42:26 up 11:10,  0 users,  load average: 0.83, 0.47, 0.29

--- npc/src/src/SRAM.scala
+++ npc/src/src/SRAM.scala
@@ -3,11 +3,11 @@ import chisel3.util._
 
 class SRAM extends Module
 {
-    val io = IO(new Bundle
-    {
-        val master = Flipped(new AXI_Lite_Master)
-        val slave  = Flipped(new AXI_Lite_Slave)
+    val master = IO(Flipped(new AXI_Lite_Master))
+    val slave  = IO(Flipped(new AXI_Lite_Slave))
         
+    val sim = IO(new Bundle
+    {
         val mm_ren = Output(Bool())
         val mm_raddr = Output(UInt(64.W))
         val mm_rdata = Input(UInt(64.W))
@@ -17,57 +17,57 @@ class SRAM extends Module
         val mm_mask = Output(UInt(8.W))
     })
 
-    io.master.aw.ready := true.B
+    master.aw.ready := true.B
     val awfire = RegInit(false.B)
     val awaddr = RegInit(0.U(64.W))
-    when (io.master.aw.fire)
+    when (master.aw.fire)
     {
         awfire := true.B
-        awaddr := io.master.aw.bits.addr
+        awaddr := master.aw.bits.addr
     }
-    .elsewhen (io.slave.b.fire)
+    .elsewhen (slave.b.fire)
     {
         awfire := false.B
     }
 
-    io.master.w.ready := true.B
+    master.w.ready := true.B
     val wfire = RegInit(false.B)
     val wdata = RegInit(0.U(64.W))
     val wstrb = RegInit(0.U(64.W))
-    when (io.master.w.fire)
+    when (master.w.fire)
     {
         wfire := true.B
-        wdata := io.master.w.bits.data
-        wstrb := io.master.w.bits.strb
+        wdata := master.w.bits.data
+        wstrb := master.w.bits.strb
     }
-    .elsewhen (io.slave.b.fire)
+    .elsewhen (slave.b.fire)
     {
         wfire := false.B
     }
 
-    io.mm_wen := awfire && wfire
-    io.mm_waddr := awaddr
-    io.mm_wdata := wdata
-    io.mm_mask := wstrb
-    io.slave.b.valid := awfire && wfire
-    io.slave.b.bits.resp := 0.U(2.W)
+    sim.mm_wen := awfire && wfire
+    sim.mm_waddr := awaddr
+    sim.mm_wdata := wdata
+    sim.mm_mask := wstrb
+    slave.b.valid := awfire && wfire
+    slave.b.bits.resp := 0.U(2.W)
 
-    io.master.ar.ready := true.B
+    master.ar.ready := true.B
     val arfire = RegInit(false.B)
     val araddr = RegInit(0.U(64.W))
-    when (io.master.ar.fire)
+    when (master.ar.fire)
     {
         arfire := true.B
-        araddr := io.master.ar.bits.addr
+        araddr := master.ar.bits.addr
     }
-    .elsewhen (io.slave.r.fire)
+    .elsewhen (slave.r.fire)
     {
         arfire := false.B
     }
 
-    io.mm_ren := arfire
-    io.mm_raddr := araddr
-    io.slave.r.valid := arfire
-    io.slave.r.bits.data := io.mm_rdata
-    io.slave.r.bits.resp := 0.U(2.W)
+    sim.mm_ren := arfire
+    sim.mm_raddr := araddr
+    slave.r.valid := arfire
+    slave.r.bits.data := mm_rdata
+    slave.r.bits.resp := 0.U(2.W)
 }
