86e0d2f06d7a235c79429a3587fc21654b6aaa7f
tracer-ysyx2205 <tracer@ysyx.org>
2023-04-22 17:05:44 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:05:44 up  8:33,  0 users,  load average: 0.42, 0.42, 0.32

--- npc/src/src/DS.scala
+++ npc/src/src/DS.scala
@@ -259,16 +259,16 @@ class DS extends Module
     io.ds_es.to_es_valid := to_es_valid
     io.ds_es.pc := pc
 
-    io.ds_es.alu_in.alu_op := alu_op
-    io.ds_es.alu_in.alu_src1 := Mux(src1_is_pc, pc, 
-        MuxCase(
-            rs1_value,
-            Seq(
-                (inst_addiw || inst_slliw || inst_sraiw || inst_addw || inst_subw || inst_sllw || inst_sraw || inst_mulw || inst_divw || inst_remw) -> Cat(Fill(32, rs1_value(31)), rs1_value(31, 0)),
-                (inst_srlw || inst_srliw || inst_divuw || inst_remuw) -> rs1_value(31, 0)
-            )
-        )
-    )
+    // io.ds_es.alu_in.alu_op := alu_op
+    // io.ds_es.alu_in.alu_src1 := Mux(src1_is_pc, pc, 
+    //     MuxCase(
+    //         rs1_value,
+    //         Seq(
+    //             (inst_addiw || inst_slliw || inst_sraiw || inst_addw || inst_subw || inst_sllw || inst_sraw || inst_mulw || inst_divw || inst_remw) -> Cat(Fill(32, rs1_value(31)), rs1_value(31, 0)),
+    //             (inst_srlw || inst_srliw || inst_divuw || inst_remuw) -> rs1_value(31, 0)
+    //         )
+    //     )
+    // )
     // io.ds_es.alu_in.alu_src2 := Mux(src2_is_imm,
     //     MuxCase(
     //         imm,
