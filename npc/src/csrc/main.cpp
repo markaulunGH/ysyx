7d6d82ffe8cdd814d230043b2d908f42c5f9891a
ysyx_22051094-Li Yanjun <liyanjun20@mails.ucas.ac.cn>
2023-05-01 12:26:56 +0800

    module test for multiplier

--- npc/src/csrc/main.cpp
+++ npc/src/csrc/main.cpp
@@ -1,55 +1,136 @@
-#include <sdb.h>
-#include <cpu.h>
-#include <sim.h>
-#include <paddr.h>
-#include <config.h>
-#include <log.h>
-#include <difftest.h>
-#include <device.h>
+// #include <sdb.h>
+// #include <cpu.h>
+// #include <sim.h>
+// #include <paddr.h>
+// #include <config.h>
+// #include <log.h>
+// #include <difftest.h>
+// #include <device.h>
 
-void init_disasm(const char *triple);
+// void init_disasm(const char *triple);
 
-int main(int argc, char** argv, char** env)
-{
-    bool batch = 0;
-    int img_size = 0, diff_so_file = 0;
-    for (int i = 1; i < argc; ++ i)
+// int main(int argc, char** argv, char** env)
+// {
+//     bool batch = 0;
+//     int img_size = 0, diff_so_file = 0;
+//     for (int i = 1; i < argc; ++ i)
+//     {
+//         if (strncmp(argv[i], "--img=", 6) == 0)
+//         {
+//             img_size = load_image(argv[i] + 6);
+//         }
+// #ifdef CONFIG_FTRACE
+//         if (strncmp(argv[i], "--elf=", 6) == 0)
+//         {
+//             init_ftrace(argv[i] + 6);
+//         }
+// #endif
+//         if (strncmp(argv[i], "--log=", 6) == 0)
+//         {
+//             init_log(argv[i] + 6);
+//         }
+//         if (strncmp(argv[i], "--batch", 7) == 0)
+//         {
+//             batch = 1;
+//         }
+//         if (strncmp(argv[i], "--diff=", 7) == 0)
+//         {
+//             diff_so_file = i;
+//         }
+//     }
+//     init_mem();
+//     init_device();
+//     init_sdb(batch);
+// #ifdef CONFIG_ITRACE
+//     init_disasm("riscv64-pc-linux-gnu");
+// #endif
+//     cpu.pc = 0x80000000;
+// #ifdef CONFIG_DIFFTEST
+//     init_difftest(argv[diff_so_file] + 7, img_size);
+// #endif
+//     init_simulation(2, argv);
+//     sdb_mainloop();
+//     end_simulation();
+//     return is_exit_status_bad();
+// }
+
+#include <memory>
+#include <verilated.h>
+#include "VTop.h"
+#include "verilated_vcd_c.h"
+#include <stdint.h>
+
+int main(int argc, char** argv, char** env) {
+    if (false && argc && argv && env) {}
+    Verilated::mkdir("logs");
+    const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+    contextp->debug(0);
+    contextp->randReset(2);
+    contextp->traceEverOn(true);
+    contextp->commandArgs(argc, argv);
+    const std::unique_ptr<VTop> top{new VTop{contextp.get(), "TOP"}};
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+
+    top->trace(tfp, 0);
+    tfp->open("logs/dump.vcd");
+
+    for (int i = 0; i < 100; ++ i)
     {
-        if (strncmp(argv[i], "--img=", 6) == 0)
-        {
-            img_size = load_image(argv[i] + 6);
-        }
-#ifdef CONFIG_FTRACE
-        if (strncmp(argv[i], "--elf=", 6) == 0)
-        {
-            init_ftrace(argv[i] + 6);
-        }
-#endif
-        if (strncmp(argv[i], "--log=", 6) == 0)
-        {
-            init_log(argv[i] + 6);
-        }
-        if (strncmp(argv[i], "--batch", 7) == 0)
-        {
-            batch = 1;
-        }
-        if (strncmp(argv[i], "--diff=", 7) == 0)
-        {
-            diff_so_file = i;
-        }
+        top->reset = i < 99;
+        tfp->dump(contextp->time());
+        contextp->timeInc(1);
+        top->clock = 0;
+        top->eval();
+        tfp->dump(contextp->time());
+        contextp->timeInc(1);
+        top->clock = 1;
     }
-    init_mem();
-    init_device();
-    init_sdb(batch);
-#ifdef CONFIG_ITRACE
-    init_disasm("riscv64-pc-linux-gnu");
-#endif
-    cpu.pc = 0x80000000;
-#ifdef CONFIG_DIFFTEST
-    init_difftest(argv[diff_so_file] + 7, img_size);
-#endif
-    init_simulation(2, argv);
-    sdb_mainloop();
-    end_simulation();
-    return is_exit_status_bad();
+
+    for (int i = 1; i < 10; ++ i)
+    {
+        __uint128_t a = rand();
+        __uint128_t b = rand();
+        int mulw = 0;
+        int sign = 3;
+
+        top->io_multiplicand = a;
+        top->io_multiplier = b;
+        top->io_mulw = mulw;
+        top->io_signed = sign;
+        top->io_in_valid = 1;
+        top->io_out_ready = 0;
+        do
+        {
+            tfp->dump(contextp->time());
+            contextp->timeInc(1);
+            top->clock = 0;
+            top->eval();
+            tfp->dump(contextp->time());
+            contextp->timeInc(1);
+            top->clock = 1;
+            top->eval();
+        } while (!top->io_in_ready);
+
+        top->io_in_valid = 0;
+        top->io_out_ready = 1;
+        do
+        {
+            tfp->dump(contextp->time());
+            contextp->timeInc(1);
+            top->clock = 0;
+            top->eval();
+            tfp->dump(contextp->time());
+            contextp->timeInc(1);
+            top->clock = 1;
+            top->eval();
+        } while (!top->io_out_valid);
+
+        assert(top->io_result_hi == (a * b) >> 64);
+        assert(top->io_result_lo == (a * b) & 0xffffffffffffffff);
+    }
+
+    top->final();
+    tfp->close();
+
+    return 0;
 }
---
+++ npc/src/src/Multiplier.scala
@@ -0,0 +1,74 @@
+import chisel3._
+import chisel3.util._
+
+class Multiplier_In extends Bundle
+{
+    val multiplicand = Output(UInt(64.W))
+    val multiplier = Output(UInt(64.W))
+    val mulw = Output(Bool())
+    val signed = Output(UInt(2.W))
+}
+
+class Multiplier_Out extends Bundle
+{
+    val result_hi = Output(UInt(64.W))
+    val result_lo = Output(UInt(64.W))
+}
+
+class Base_Multipiler extends Module
+{
+    val in = IO(Flipped(Decoupled(new Multiplier_In)))
+    val out = IO(Decoupled(new Multiplier_Out))
+    val io = IO(new Bundle
+    {
+        val flush = Input(Bool())
+    })
+}
+
+class Multiplier extends Base_Multipiler
+{
+    val s_idle :: s_calc :: s_finish :: Nil = Enum(3)
+    val state = RegInit(s_idle)
+
+    val finish = Wire(Bool())
+    state := MuxLookup(state, s_idle, Seq(
+        s_idle -> Mux(in.fire && !io.flush, s_calc, s_idle),
+        s_calc -> MuxCase(s_calc, Seq(
+            io.flush -> s_idle,
+            finish -> s_finish
+        )),
+        s_finish -> Mux(out.fire || io.flush, s_idle, s_finish)
+    ))
+    in.ready := state === s_idle
+    out.valid := state === s_finish
+
+    val x = RegInit(0.U(64.W))
+    val y = RegInit(0.U(64.W))
+    val res = RegInit(0.U(128.W))
+
+    when (state === s_idle && in.fire && !io.flush)
+    {
+        x := in.bits.multiplicand
+        y := Cat(in.bits.multiplier, 0.U(1.W))
+        res := 0.U(128.W)
+    }
+    .elsewhen (state === s_calc)
+    {
+        y := y >> 2
+        x := x << 2
+        res := MuxCase(0.U(64.W), Seq(
+            (y(2, 0) === 0.U(3.W)) -> (res),
+            (y(2, 0) === 1.U(3.W)) -> (res + x),
+            (y(2, 0) === 2.U(3.W)) -> (res + x),
+            (y(2, 0) === 3.U(3.W)) -> (res + (x << 1)),
+            (y(2, 0) === 4.U(3.W)) -> (res - (x << 1)),
+            (y(2, 0) === 5.U(3.W)) -> (res - (x << 2)),
+            (y(2, 0) === 6.U(3.W)) -> (res - (x << 2)),
+            (y(2, 0) === 7.U(3.W)) -> (res)
+        ))
+    }
+    finish := y.orR === 0.U(1.W)
+
+    out.bits.result_hi := res(127, 64)
+    out.bits.result_lo := res(63, 0)
+}
