cac269a37042c3129cf6cebcd6bd0b9df1873cb3
ysyx_22051094-Li Yanjun <liyanjun20@mails.ucas.ac.cn>
2023-05-02 10:17:14 +0800

    fix rand

--- npc/src/csrc/main.cpp
+++ npc/src/csrc/main.cpp
@@ -62,8 +62,12 @@
 
 uint64_t _random()
 {
-    return static_cast<uint64_t>(rand()) << 32 | rand();
+    return static_cast<uint64_t>(rand()) << 33 | static_cast<uint64_t>(rand()) << 2 | rand() & 3;
 }
+
+#define BITMASK(bits) ((1ull << (bits)) - 1)
+#define BITS(x, hi, lo) (((x) >> (lo)) & BITMASK((hi) - (lo) + 1))
+#define SEXT(x, len) ({ struct { int64_t n : len; } __x = { .n = x }; (uint64_t)__x.n; })
 
 int main(int argc, char** argv, char** env) {
     if (false && argc && argv && env) {}
@@ -96,11 +100,8 @@ int main(int argc, char** argv, char** env) {
 
     for (int i = 1; i < 1e5; ++ i)
     {
-        uint64_t a_64 = _random();
-        uint64_t b_64 = _random();
-        uint32_t a_32 = _random() & 0xffffffff;
-        uint32_t b_32 = _random() & 0xffffffff;
-        int mulw = _random() & 1;
+        uint64_t a = _random();
+        uint64_t b = _random();
         int sign = _random() & 3;
         while (sign == 1)
         {
@@ -109,14 +110,19 @@ int main(int argc, char** argv, char** env) {
         __uint128_t res;
         switch (sign)
         {
-            case 0: res = static_cast<__uint128_t>(static_cast<__uint128_t>(mulw ? a_32 : a_64) * static_cast<__uint128_t>(mulw ? b_32 : b_64)); break;
-            case 2: res = static_cast<__uint128_t>(static_cast<__int128_t >(mulw ? a_32 : a_64) * static_cast<__uint128_t>(mulw ? b_32 : b_64)); break;
-            case 3: res = static_cast<__uint128_t>(static_cast<__int128_t >(mulw ? a_32 : a_64) * static_cast<__int128_t >(mulw ? b_32 : b_64)); break;
+            case 0:
+                res = static_cast<__uint128_t>(static_cast<__uint128_t>(a) * static_cast<__uint128_t>(b));
+                break;
+            case 2:
+                res = static_cast<__uint128_t>(static_cast<__int128_t >(static_cast<int64_t>(a)) * static_cast<__uint128_t>(b));
+                break;
+            case 3:
+                res = static_cast<__uint128_t>(static_cast<__int128_t >(static_cast<int64_t>(a)) * static_cast<__int128_t >(static_cast<int64_t>(b)));
+                break;
         }
 
-        top->io_multiplicand = mulw ? a_32 : a_64;
-        top->io_multiplier = mulw ? b_32 : b_64;
-        top->io_mulw = mulw;
+        top->io_multiplicand = a;
+        top->io_multiplier = b;
         top->io_signed = sign;
         top->io_in_valid = 1;
         top->io_out_ready = 0;
@@ -153,10 +159,9 @@ int main(int argc, char** argv, char** env) {
         if (top->io_result_hi != static_cast<uint64_t>(res >> 64) || top->io_result_lo != static_cast<uint64_t>(res & 0xffffffffffffffff))
         {
             printf("FAIL\n");
-            printf("mulw:     %d\n", mulw);
             printf("sign:     %d\n", sign);
-            printf("a:        %016lx\n", static_cast<uint64_t>(mulw ? a_32 : a_64) & 0xffffffffffffffff);
-            printf("b:        %016lx\n", static_cast<uint64_t>(mulw ? b_32 : b_64) & 0xffffffffffffffff);
+            printf("a:        %016lx\n", a);
+            printf("b:        %016lx\n", b);
             printf("expected: %016lx %016lx\n", static_cast<uint64_t>(res >> 64), static_cast<uint64_t>(res & 0xffffffffffffffff));
             printf("got:      %016lx %016lx\n", top->io_result_hi, top->io_result_lo);
             top->final();
