5869c79d7a22876dc10aeb0b4c17eee7ecbe95b0
tracer-ysyx2205 <tracer@ysyx.org>
2023-05-03 09:03:42 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:03:42 up 44 min,  1 user,  load average: 0.25, 0.12, 0.04

--- npc/src/src/Divider.scala
+++ npc/src/src/Divider.scala
@@ -49,8 +49,8 @@ class Divider extends Base_Divider
 
     when (state === s_idle && in.fire && !io.flush)
     {
-        x := Cat(0.U(64.W), Mux(io.bits.sign && in.bits.dividend(63), ~in.bits.dividend + 1.U(64.W), in.bits.dividend))
-        y := Cat(0.U(64.W), Mux(io.bits.sign && in.bits.divisor(63), ~in.bits.divisor + 1.U(64.W), in.bits.divisor))
+        x := Cat(0.U(64.W), Mux(in.bits.sign && in.bits.dividend(63), ~in.bits.dividend + 1.U(64.W), in.bits.dividend))
+        y := Cat(0.U(64.W), Mux(in.bits.sign && in.bits.divisor(63), ~in.bits.divisor + 1.U(64.W), in.bits.divisor))
         sign := Cat(in.bits.dividend(63), in.bits.divisor(63)) & in.bits.signed
         cnt := 0.U(6.W)
     }
