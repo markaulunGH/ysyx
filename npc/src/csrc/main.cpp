a2c6cf27e3a27446271f789f1625395046070a38
ysyx_22051094-Li Yanjun <liyanjun20@mails.ucas.ac.cn>
2023-05-03 09:04:22 +0800

    complete module test for divider

--- npc/src/csrc/main.cpp
+++ npc/src/csrc/main.cpp
@@ -1,178 +1,169 @@
-#include <sdb.h>
-#include <cpu.h>
-#include <sim.h>
-#include <paddr.h>
-#include <config.h>
-#include <log.h>
-#include <difftest.h>
-#include <device.h>
+// #include <sdb.h>
+// #include <cpu.h>
+// #include <sim.h>
+// #include <paddr.h>
+// #include <config.h>
+// #include <log.h>
+// #include <difftest.h>
+// #include <device.h>
 
-void init_disasm(const char *triple);
+// void init_disasm(const char *triple);
 
-int main(int argc, char** argv, char** env)
+// int main(int argc, char** argv, char** env)
+// {
+//     bool batch = 0;
+//     int img_size = 0, diff_so_file = 0;
+//     for (int i = 1; i < argc; ++ i)
+//     {
+//         if (strncmp(argv[i], "--img=", 6) == 0)
+//         {
+//             img_size = load_image(argv[i] + 6);
+//         }
+// #ifdef CONFIG_FTRACE
+//         if (strncmp(argv[i], "--elf=", 6) == 0)
+//         {
+//             init_ftrace(argv[i] + 6);
+//         }
+// #endif
+//         if (strncmp(argv[i], "--log=", 6) == 0)
+//         {
+//             init_log(argv[i] + 6);
+//         }
+//         if (strncmp(argv[i], "--batch", 7) == 0)
+//         {
+//             batch = 1;
+//         }
+//         if (strncmp(argv[i], "--diff=", 7) == 0)
+//         {
+//             diff_so_file = i;
+//         }
+//     }
+//     init_mem();
+//     init_device();
+//     init_sdb(batch);
+// #ifdef CONFIG_ITRACE
+//     init_disasm("riscv64-pc-linux-gnu");
+// #endif
+//     cpu.pc = 0x80000000;
+// #ifdef CONFIG_DIFFTEST
+//     init_difftest(argv[diff_so_file] + 7, img_size);
+// #endif
+//     init_simulation(2, argv);
+//     sdb_mainloop();
+//     end_simulation();
+//     return is_exit_status_bad();
+// }
+
+#include <memory>
+#include <verilated.h>
+#include "VTop.h"
+#include "verilated_vcd_c.h"
+#include <stdint.h>
+
+uint64_t _random()
 {
-    bool batch = 0;
-    int img_size = 0, diff_so_file = 0;
-    for (int i = 1; i < argc; ++ i)
-    {
-        if (strncmp(argv[i], "--img=", 6) == 0)
-        {
-            img_size = load_image(argv[i] + 6);
-        }
-#ifdef CONFIG_FTRACE
-        if (strncmp(argv[i], "--elf=", 6) == 0)
-        {
-            init_ftrace(argv[i] + 6);
-        }
-#endif
-        if (strncmp(argv[i], "--log=", 6) == 0)
-        {
-            init_log(argv[i] + 6);
-        }
-        if (strncmp(argv[i], "--batch", 7) == 0)
-        {
-            batch = 1;
-        }
-        if (strncmp(argv[i], "--diff=", 7) == 0)
-        {
-            diff_so_file = i;
-        }
-    }
-    init_mem();
-    init_device();
-    init_sdb(batch);
-#ifdef CONFIG_ITRACE
-    init_disasm("riscv64-pc-linux-gnu");
-#endif
-    cpu.pc = 0x80000000;
-#ifdef CONFIG_DIFFTEST
-    init_difftest(argv[diff_so_file] + 7, img_size);
-#endif
-    init_simulation(2, argv);
-    sdb_mainloop();
-    end_simulation();
-    return is_exit_status_bad();
+    return static_cast<uint64_t>(rand()) << 33 | static_cast<uint64_t>(rand()) << 2 | rand() & 3;
 }
 
-// #include <memory>
-// #include <verilated.h>
-// #include "VTop.h"
-// #include "verilated_vcd_c.h"
-// #include <stdint.h>
+int main(int argc, char** argv, char** env) {
+    if (false && argc && argv && env) {}
+    Verilated::mkdir("logs");
+    const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
+    contextp->debug(0);
+    contextp->randReset(2);
+    contextp->traceEverOn(true);
+    contextp->commandArgs(argc, argv);
+    const std::unique_ptr<VTop> top{new VTop{contextp.get(), "TOP"}};
+    VerilatedVcdC* tfp = new VerilatedVcdC;
 
-// uint64_t _random()
-// {
-//     return static_cast<uint64_t>(rand()) << 33 | static_cast<uint64_t>(rand()) << 2 | rand() & 3;
-// }
+    top->trace(tfp, 0);
+    tfp->open("logs/dump.vcd");
 
-// #define BITMASK(bits) ((1ull << (bits)) - 1)
-// #define BITS(x, hi, lo) (((x) >> (lo)) & BITMASK((hi) - (lo) + 1))
-// #define SEXT(x, len) ({ struct { int64_t n : len; } __x = { .n = x }; (uint64_t)__x.n; })
+    for (int i = 0; i < 100; ++ i)
+    {
+        top->reset = i < 99;
+        tfp->dump(contextp->time());
+        contextp->timeInc(1);
+        top->clock = 0;
+        top->eval();
+        tfp->dump(contextp->time());
+        contextp->timeInc(1);
+        top->clock = 1;
+        top->eval();
+    }
 
-// int main(int argc, char** argv, char** env) {
-//     if (false && argc && argv && env) {}
-//     Verilated::mkdir("logs");
-//     const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
-//     contextp->debug(0);
-//     contextp->randReset(2);
-//     contextp->traceEverOn(true);
-//     contextp->commandArgs(argc, argv);
-//     const std::unique_ptr<VTop> top{new VTop{contextp.get(), "TOP"}};
-//     VerilatedVcdC* tfp = new VerilatedVcdC;
+    top->io_flush = 0;
 
-//     top->trace(tfp, 0);
-//     tfp->open("logs/dump.vcd");
+    for (int i = 1; i < 1e5; ++ i)
+    {
+        uint64_t a = _random();
+        uint64_t b = _random();
+        int sign = 0;
+        uint64_t quotient;
+        uint64_t remainder;
+        if (sign)
+        {
+            quotient = static_cast<int64_t>(a) / static_cast<int64_t>(b);
+            remainder = static_cast<int64_t>(a) % static_cast<int64_t>(b);
+        }
+        else
+        {
+            quotient = a / b;
+            remainder = a % b;
+        }
 
-//     for (int i = 0; i < 1e5; ++ i)
-//     {
-//         top->reset = i < 99;
-//         tfp->dump(contextp->time());
-//         contextp->timeInc(1);
-//         top->clock = 0;
-//         top->eval();
-//         tfp->dump(contextp->time());
-//         contextp->timeInc(1);
-//         top->clock = 1;
-//         top->eval();
-//     }
+        top->io_dividend = a;
+        top->io_divisor = b;
+        top->io_signed = sign;
+        top->io_in_valid = 1;
+        top->io_out_ready = 0;
 
-//     top->io_flush = 0;
+        do
+        {
+            top->eval();
+            tfp->dump(contextp->time());
+            contextp->timeInc(1);
+            top->clock = 0;
+            top->eval();
+            tfp->dump(contextp->time());
+            contextp->timeInc(1);
+            top->clock = 1;
+        } while (!top->io_in_ready);
+        top->eval();
 
-//     for (int i = 1; i < 1e5; ++ i)
-//     {
-//         uint64_t a = _random();
-//         uint64_t b = _random();
-//         int sign = _random() & 3;
-//         while (sign == 1)
-//         {
-//             sign = _random() & 3;
-//         }
-//         __uint128_t res;
-//         switch (sign)
-//         {
-//             case 0:
-//                 res = static_cast<__uint128_t>(static_cast<__uint128_t>(a) * static_cast<__uint128_t>(b));
-//                 break;
-//             case 2:
-//                 res = static_cast<__uint128_t>(static_cast<__int128_t >(static_cast<int64_t>(a)) * static_cast<__uint128_t>(b));
-//                 break;
-//             case 3:
-//                 res = static_cast<__uint128_t>(static_cast<__int128_t >(static_cast<int64_t>(a)) * static_cast<__int128_t >(static_cast<int64_t>(b)));
-//                 break;
-//         }
+        top->io_in_valid = 0;
+        top->io_out_ready = 1;
 
-//         top->io_multiplicand = a;
-//         top->io_multiplier = b;
-//         top->io_signed = sign;
-//         top->io_in_valid = 1;
-//         top->io_out_ready = 0;
-
-//         do
-//         {
-//             top->eval();
-//             tfp->dump(contextp->time());
-//             contextp->timeInc(1);
-//             top->clock = 0;
-//             top->eval();
-//             tfp->dump(contextp->time());
-//             contextp->timeInc(1);
-//             top->clock = 1;
-//         } while (!top->io_in_ready);
-//         top->eval();
-
-//         top->io_in_valid = 0;
-//         top->io_out_ready = 1;
-
-//         do
-//         {
-//             top->eval();
-//             tfp->dump(contextp->time());
-//             contextp->timeInc(1);
-//             top->clock = 0;
-//             top->eval();
-//             tfp->dump(contextp->time());
-//             contextp->timeInc(1);
-//             top->clock = 1;
-//         } while (!top->io_out_valid);
-//         top->eval();
+        do
+        {
+            top->eval();
+            tfp->dump(contextp->time());
+            contextp->timeInc(1);
+            top->clock = 0;
+            top->eval();
+            tfp->dump(contextp->time());
+            contextp->timeInc(1);
+            top->clock = 1;
+        } while (!top->io_out_valid);
+        top->eval();
         
-//         if (top->io_result_hi != static_cast<uint64_t>(res >> 64) || top->io_result_lo != static_cast<uint64_t>(res & 0xffffffffffffffff))
-//         {
-//             printf("FAIL\n");
-//             printf("sign:     %d\n", sign);
-//             printf("a:        %016lx\n", a);
-//             printf("b:        %016lx\n", b);
-//             printf("expected: %016lx %016lx\n", static_cast<uint64_t>(res >> 64), static_cast<uint64_t>(res & 0xffffffffffffffff));
-//             printf("got:      %016lx %016lx\n", top->io_result_hi, top->io_result_lo);
-//             top->final();
-//             tfp->close();
-//             return 0;
-//         }
-//     }
+        if (top->io_quotient != quotient || top->io_remainder != remainder)
+        {
+            printf("FAIL\n");
+            printf("sign:     %d\n", sign);
+            printf("a:        %016lx\n", a);
+            printf("b:        %016lx\n", b);
+            printf("expected: %016lx %016lx\n", quotient, remainder);
+            printf("got:      %016lx %016lx\n", top->io_quotient, top->io_remainder);
+            top->final();
+            tfp->close();
+            return 0;
+        }
+    }
 
-//     top->final();
-//     tfp->close();
-//     printf("PASS\n");
+    top->final();
+    tfp->close();
+    printf("PASS\n");
 
-//     return 0;
-// }
+    return 0;
+}
---
+++ npc/src/src/Divider.scala
@@ -0,0 +1,67 @@
+import chisel3._
+import chisel3.util._
+
+class Divider_In extends Bundle
+{
+    val dividend = Output(UInt(64.W))
+    val divisor = Output(UInt(64.W))
+    val signed = Output(Bool())
+}
+
+class Divider_Out extends Bundle
+{
+    val quotient = Output(UInt(64.W))
+    val remainder = Output(UInt(64.W))
+}
+
+class Base_Divider extends Module
+{
+    val in = IO(Flipped(Decoupled(new Divider_In)))
+    val out = IO(Decoupled(new Divider_Out))
+    val io = IO(new Bundle
+    {
+        val flush = Input(Bool())
+    })
+}
+
+class Divider extends Base_Divider
+{
+    val s_idle :: s_calc :: s_finish :: Nil = Enum(3)
+    val state = RegInit(s_idle)
+
+    val finish = Wire(Bool())
+    state := MuxLookup(state, s_idle, Seq(
+        s_idle -> Mux(in.fire && !io.flush, s_calc, s_idle),
+        s_calc -> MuxCase(s_calc, Seq(
+            io.flush -> s_idle,
+            finish -> s_finish
+        )),
+        s_finish -> Mux(out.fire || io.flush, s_idle, s_finish)
+    ))
+    in.ready := state === s_idle
+    out.valid := state === s_finish
+
+    val x = RegInit(0.U(128.W))
+    val y = RegInit(0.U(65.W))
+    val sign = RegInit(0.U(2.W))
+    val quotient = RegInit(0.U(64.W))
+    val cnt = RegInit(0.U(6.W))
+
+    when (state === s_idle && in.fire && !io.flush)
+    {
+        x := Cat(0.U(64.W), Mux(in.bits.signed && in.bits.dividend(63), ~in.bits.dividend + 1.U(64.W), in.bits.dividend))
+        y := Cat(0.U(64.W), Mux(in.bits.signed && in.bits.divisor(63), ~in.bits.divisor + 1.U(64.W), in.bits.divisor))
+        sign := Cat(in.bits.dividend(63), in.bits.divisor(63)) & in.bits.signed
+        cnt := 0.U(6.W)
+    }
+    .elsewhen (state === s_calc)
+    {
+        quotient := quotient << 1 | (y < x(127, 63))
+        x := Mux(y < x(127, 63), (x - Cat(y, 0.U(63.W))) << 1, x << 1)
+        cnt := cnt + 1.U(6.W)
+    }
+    finish := cnt === 63.U(6.W)
+
+    out.bits.quotient := Mux(sign.xorR, ~quotient + 1.U(64.W), quotient)
+    out.bits.remainder := Mux(sign(1), ~x(127, 64) + 1.U(64.W), x(127, 64))
+}
