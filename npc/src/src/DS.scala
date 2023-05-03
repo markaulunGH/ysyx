94e6b46063c05f22a9b4aa30db085a766a367c01
tracer-ysyx2205 <tracer@ysyx.org>
2023-05-03 10:11:20 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:11:20 up  1:51,  1 user,  load average: 0.34, 0.10, 0.03

--- npc/src/src/DS.scala
+++ npc/src/src/DS.scala
@@ -231,10 +231,10 @@ class DS extends Module
         (rf_r.raddr2 =/= 0.U && ws_ds.ws_valid    && ws_ds.rf_wen && rf_r.raddr2 === ws_ds.rf_waddr) -> ws_ds.rf_wdata
     ))
 
-    val rf1_hazard := (es_ds.es_valid && (es_ds.mm_ren || es_ds.csr_wen) && rf_r.raddr1 === es_ds.rf_waddr ||
+    val rf1_hazard = (es_ds.es_valid && (es_ds.mm_ren || es_ds.csr_wen) && rf_r.raddr1 === es_ds.rf_waddr ||
                    ms_ds.ms_valid && ((ms_ds.mm_ren && !ms_ds.to_ws_valid) || ms_ds.csr_wen) && rf_r.raddr1 === ms_ds.rf_waddr) &&
                    rf_r.raddr1 =/= 0.U
-    val rf2_hazard := (es_ds.es_valid && (es_ds.mm_ren || es_ds.csr_wen) && rf_r.raddr2 === es_ds.rf_waddr ||
+    val rf2_hazard = (es_ds.es_valid && (es_ds.mm_ren || es_ds.csr_wen) && rf_r.raddr2 === es_ds.rf_waddr ||
                    ms_ds.ms_valid && ((ms_ds.mm_ren || !ms_ds.to_ws_valid) || ms_ds.csr_wen) && rf_r.raddr1 === ms_ds.rf_waddr) &&
                    rf_r.raddr2 =/= 0.U
     hazard := read_rf1 && rf1_hazard || read_rf2 && rf2_hazard
