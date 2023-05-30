161b5539b474aead4a68840a0844db94f12a6181
tracer-ysyx2205 <tracer@ysyx.org>
2023-05-30 16:34:29 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:34:29 up 50 min,  1 user,  load average: 0.00, 0.04, 0.00

--- npc/src/csrc/include/device.h
+++ npc/src/csrc/include/device.h
@@ -10,6 +10,7 @@ void init_device();
 
 #define SERIAL_PORT 0xa00003f8
 #define RTC_ADDR    0xa0000048
+#define KBD_ADDR    0xa0000060
 #define VGACTL_ADDR 0xa0000100
 #define FB_ADDR     0xa1000000
 
