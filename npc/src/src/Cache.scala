75be15d1fd263ab7db47d21556887cd2b35c3c4b
tracer-ysyx2205 <tracer@ysyx.org>
2023-06-07 23:12:34 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:12:34 up 12:34,  1 user,  load average: 0.04, 0.14, 0.08

--- npc/src/src/Cache.scala
+++ npc/src/src/Cache.scala
@@ -12,7 +12,7 @@ class Cache_Sram(n : Int, width : Int) extends Module
         val wdata = Input(UInt(width.W))
     })
 
-    val rf[256] = Reg(Vec(256, UInt(1.W)))
+    val rf = RegInit(VecInit(Seq.fill(256)(0.U(1.W))))
     rf[io.addr] := io.wdata
     io.rdata := rf[io.addr]
 }
