b35327ecaa8b236292000874eafa30417b5554e8
tracer-ysyx2205 <tracer@ysyx.org>
2023-06-07 15:37:53 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:37:53 up  4:59,  1 user,  load average: 0.03, 0.05, 0.01

--- npc/src/src/Cache.scala
+++ npc/src/src/Cache.scala
@@ -2,6 +2,13 @@ import chisel3._
 import chisel3.util._
 import chisel3.util.random
 
+import chisel3._
+import chisel3.util._
+import chipsalliance.rocketchip.config.Parameters
+import freechips.rocketchip.diplomacy.AddressSet
+import utils._
+import utility._
+
 class Base_Cache extends Module
 {
 
