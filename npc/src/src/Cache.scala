3b17ceb1a4831d1930aff03f993eb5a58a6530db
tracer-ysyx2205 <tracer@ysyx.org>
2023-06-07 18:32:12 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:32:12 up  7:54,  1 user,  load average: 0.00, 0.00, 0.00

--- npc/src/src/Cache.scala
+++ npc/src/src/Cache.scala
@@ -9,5 +9,13 @@ class Base_Cache extends Module
 
 class Cache extends Base_Cache
 {
-    val pseudoRandomNumber = LFSR(1)
+    val pseudoRandomNumber = LFSR16()
+
+    val s_idle :: s_lookup :: s_miss :: s_replace :: s_refill :: Nil = Enum(5)
+
+    // state := MuxLookup(state, s_idle, Seq(
+    //     s_idle -> 
+    // ))
+    
+    val s_wbidle :: s_wbwrite :: Nil = Enum(2)
 }
