0e7af4360322ea2da2adbcbb9492335d0b8096ca
tracer-ysyx2205 <tracer@ysyx.org>
2023-06-08 10:49:00 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:49:00 up 46 min,  1 user,  load average: 0.00, 0.00, 0.00

--- npc/src/src/Cache.scala
+++ npc/src/src/Cache.scala
@@ -27,26 +27,40 @@ class Cache(way : Int) extends Module
     val s_idle :: s_lookup :: s_miss :: s_replace :: s_refill :: Nil = Enum(5)
     val state = RegInit(s_idle)
 
+    val hazard = Wire(Bool())
+    val cache_hit = Wire(Bool())
+    val write_finish = Wire(Bool())
+    val read_finish = Wire(Bool())
+    val cnt = RegInit(0.U(2.W))
     state := MuxLookup(state, s_idle, Seq(
-        s_idle -> Mux(valid || !hazard, s_lookup, s_idle),
-        s_lookup -> Mux(cache_hit, Mux(valid && !hazard, s_lookup, s_idle), s_miss),
-        s_miss -> Mux(master.aw.ready, s_replace, s_miss),
-        s_replace -> Mux(master.ar.ready, s_refill, s_replace),
+        s_idle -> Mux(cpu_master.fire() || !hazard, s_lookup, s_idle),
+        s_lookup -> Mux(cache_hit, Mux(cpu_master.fire() && !hazard, s_lookup, s_idle), s_miss),
+        // what if cacheline is invalid?
+        s_miss -> Mux(master.aw.fire(), s_replace, s_miss),
+        s_replace -> Mux(master.ar.fire(), s_refill, s_replace),
         s_refill -> Mux(master.w.ready, s_idle, s_refill)
     ))
+
+    val cpu_master_reg = RegEnable(cpu_master, state === s_idle)
+
+    val hit = Wire(Vec(way, Bool()))
+    for (i <- 0 until way)
+    {
+        // hit(i) = tagV(i)(0) && 
+    }
 
     val s_wbidle :: s_wbwrite :: Nil = Enum(2)
     val wb_state = RegInit(s_wbidle)
 
-    wb_state := MuxLookup(wb_state, s_wbidle, Seq(
-        s_wbidle -> Mux(state === s_lookup && cache_hit ?, s_wbwrite, s_wbidle),
-        s_wbwrite -> Mux(state === s_lookup && cache_hit ?, s_wbwrite, s_idle)
-    ))
+    // wb_state := MuxLookup(wb_state, s_wbidle, Seq(
+    //     s_wbidle -> Mux(state === s_lookup && cache_hit ?, s_wbwrite, s_wbidle),
+    //     s_wbwrite -> Mux(state === s_lookup && cache_hit ?, s_wbwrite, s_idle)
+    // ))
 
     val pseudoRandomNumber = LFSR(way - 1)
 
     for (i <- 0 to way)
     {
-        
+        val tagV = new Cache_Sram(32, 1024)
     }
 }
