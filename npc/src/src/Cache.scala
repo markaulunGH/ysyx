12cae38841a02f82bc640bfc34977ede303e7647
tracer-ysyx2205 <tracer@ysyx.org>
2023-06-09 22:00:24 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:00:24 up 59 min,  1 user,  load average: 0.00, 0.01, 0.00

--- npc/src/src/Cache.scala
+++ npc/src/src/Cache.scala
@@ -17,51 +17,41 @@ class Cache_Sram(width : Int, depth : Int) extends Module
     io.din := rf(io.addr)
 }
 
-class Cache(way : Int, depth : Int) extends Module
+class Way extends Bundle
+{
+    val tagV = new Cache_Sram(52, 256)
+    val dirty = new Cache_Sram(1, 256)
+    val data0 = new Cache_Sram(64, 256)
+    val data1 = new Cache_Sram(64, 256)
+    val data2 = new Cache_Sram(64, 256)
+    val data3 = new Cache_Sram(64, 256)
+}
+
+class Cache extends Module
 {
     val cpu_master = IO(Flipped(new AXI_Lite_Master))
     val cpu_slave  = IO(Flipped(new AXI_Lite_Slave))
     val master     = IO(new AXI_Lite_Master)
     val slave      = IO(new AXI_Lite_Slave)
 
-    val s_idle :: s_lookup :: s_miss :: s_replace :: s_refill :: Nil = Enum(5)
+    val s_idle :: s_lookup :: s_aw :: s_w :: s_ar :: s_r :: Nil = Enum(5)
     val state = RegInit(s_idle)
 
+    val cache_req = cpu_master.ar.valid || cpu_master.aw.valid
     val hazard = Wire(Bool())
     val cache_hit = Wire(Bool())
     val write_finish = Wire(Bool())
     val read_finish = Wire(Bool())
     val cnt = RegInit(0.U(2.W))
-    val cache_req = Wire(Bool())
+
     state := MuxLookup(state, s_idle, Seq(
         s_idle -> Mux(cache_req && !hazard, s_lookup, s_idle),
-        s_lookup -> Mux(cache_hit, Mux(cache_req && !hazard, s_lookup, s_idle), s_miss),
+        s_lookup -> Mux(cache_hit, Mux(cache_req && !hazard, s_lookup, s_idle), s_aw),
         // what if cacheline is invalid?
-        s_miss -> Mux(master.aw.fire, s_replace, s_miss),
-        s_replace -> Mux(master.ar.fire, s_refill, s_replace),
-        s_refill -> Mux(master.w.ready, s_idle, s_refill)
     ))
 
     val cpu_master_reg = RegEnable(cpu_master, state === s_idle)
 
-    val hit = Wire(Vec(way, Bool()))
-    for (i <- 0 until way)
-    {
-        // hit(i) = tagV(i)(0) && 
-    }
-
-    val s_wbidle :: s_wbwrite :: Nil = Enum(2)
-    val wb_state = RegInit(s_wbidle)
-
-    // wb_state := MuxLookup(wb_state, s_wbidle, Seq(
-    //     s_wbidle -> Mux(state === s_lookup && cache_hit ?, s_wbwrite, s_wbidle),
-    //     s_wbwrite -> Mux(state === s_lookup && cache_hit ?, s_wbwrite, s_idle)
-    // ))
-
-    val pseudoRandomNumber = LFSR(way - 1)
-
-    for (i <- 0 to way)
-    {
-        val tagV = new Cache_Sram(32, 1024)
-    }
+    val way0 = new Way
+    val way1 = new Way
 }
