e006a44b1692ace8b76f2e7964013c4f6183f0e0
tracer-ysyx2205 <tracer@ysyx.org>
2023-06-09 22:20:04 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:20:04 up  1:19,  1 user,  load average: 0.00, 0.00, 0.00

--- npc/src/src/Cache.scala
+++ npc/src/src/Cache.scala
@@ -34,24 +34,46 @@ class Cache extends Module
     val master     = IO(new AXI_Lite_Master)
     val slave      = IO(new AXI_Lite_Slave)
 
+    val req = new Bundle
+    {
+        val valid  = cpu_master.ar.valid || cpu_master.aw.valid
+        val op     = cpu_master.aw.valid
+        val tag    = Mux(op, cpu_master.aw.bit.addr(63, 13), cpu_master.ar.bits.addr(63, 13))
+        val index  = Mux(op, cpu_master.aw.bits.addr(12, 5), cpu_master.ar.bits.addr(12, 5))
+        val offset = Mux(op, cpu_master.aw.bits.addr(4, 0), cpu_master.ar.bits.addr(4, 0))
+    }
+
+    val random_bit = LFSR(1)
+
     val s_idle :: s_lookup :: s_aw :: s_w :: s_ar :: s_r :: Nil = Enum(5)
     val state = RegInit(s_idle)
 
-    val cache_req = cpu_master.ar.valid || cpu_master.aw.valid
     val hazard = Wire(Bool())
-    val cache_hit = Wire(Bool())
+    val hit = Wire(Bool())
     val write_finish = Wire(Bool())
     val read_finish = Wire(Bool())
     val cnt = RegInit(0.U(2.W))
 
     state := MuxLookup(state, s_idle, Seq(
-        s_idle -> Mux(cache_req && !hazard, s_lookup, s_idle),
-        s_lookup -> Mux(cache_hit, Mux(cache_req && !hazard, s_lookup, s_idle), s_aw),
-        // what if cacheline is invalid?
+        s_idle -> Mux(req.valid && !hazard, s_lookup, s_idle),
+        s_lookup -> Mux(hit, Mux(cache_req && !hazard, s_lookup, s_idle), s_aw),
+        s_aw -> Mux(master.aw.fire, s_w, s_aw),
+        s_w -> Mux(slave.w.fire, Mux(cnt === 2.U, s_ar, s_aw), s_w),
+        s_ar -> Mux(master.ar.fire, s_r, s_ar),
+        s_r -> Mux(slave.r.fire, Mux(cnt === 2.U, s_idle, s_r), s_r)
     ))
 
-    val cpu_master_reg = RegEnable(cpu_master, state === s_idle)
+    val req_reg = RegEnable(req, state === s_idle)
+    val way_sel_reg = RegEnable(random_bit, state === s_lookup)
+    val state_reg = RegNext(state)
 
     val way0 = new Way
     val way1 = new Way
+    val way = Mux(way_sel_reg, way1, way0)
+
+    val hit_way0 = way0.tagV.io.dout(0) && way0.tagV.io.dout(51, 1) === req_reg.tag
+    val hit_way1 = way1.tagV.io.dout(0) && way1.tagV.io.dout(51, 1) === req_reg.tag
+    hit := hit_way0 || hit_way1
+
+    master.aw.bits.addr = Cat()
 }
