bb5084b8098474af81c1223e8e14e7fc2ec273f6
tracer-ysyx2205 <tracer@ysyx.org>
2023-06-12 14:42:06 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:42:06 up 20 min,  1 user,  load average: 0.42, 0.33, 0.13

--- npc/src/src/Top.scala
+++ npc/src/src/Top.scala
@@ -62,8 +62,6 @@ class Top extends Module
         val rf = Output(Vec(32, UInt(64.W)))
         val rf_wen = Output(Bool())
         val mm_pc = Output(UInt(64.W))
-
-        val hit = Output(Bool())
     })
     
     val pf = Module(new PF)
@@ -95,7 +93,11 @@ class Top extends Module
     dcache.cpu_master <> es.data_master
     dcache.cpu_slave  <> ms.data_slave
 
-    io.hit := icache.debug.hit
+    val debug = IO(new Bundle
+    {
+        val hit = Output(Bool())
+    })
+    debug <> icache.debug
     
     val arbiter = Module(new AXI_Arbiter)
     arbiter.inst_master <> icache.master
