a8e4b68da8b93b9b1323dd5e334285021c601dc2
ysyx_22051094-Li Yanjun <liyanjun20@mails.ucas.ac.cn>
2023-06-12 12:33:40 +0800

    feat: add config for vcd and fst

--- npc/src/csrc/include/sim.h
+++ npc/src/csrc/include/sim.h
@@ -1,14 +1,23 @@
 #ifndef __SIM_H__
 #define __SIM_H__
 
+#include <config.h>
 #include "VTop.h"
+#ifdef CONFIG_WAVE_FST
 #include "verilated_fst_c.h"
+#else
+#include "verilated_vcd_c.h"
+#endif
 #include <verilated.h>
 #include <nvboard.h>
 
 extern const std::unique_ptr<VerilatedContext> contextp;
 extern const std::unique_ptr<VTop> top;
+#ifdef CONFIG_WAVE_FST
 extern VerilatedFstC* tfp;
+#else
+extern VerilatedVcdC* tfp;
+#endif
 
 void init_simulation(int argc, char** argv);
 void end_simulation();
