83390bd9f519a5a7a15bcc2dac4ed5888d00a2dc
ysyx_22051094-Li Yanjun <liyanjun20@mails.ucas.ac.cn>
2023-06-12 12:26:17 +0800

    fix: don't need this anymore

--- npc/src/csrc/cpu/cpu-exec.cpp
+++ npc/src/csrc/cpu/cpu-exec.cpp
@@ -190,10 +190,6 @@ static void trace_and_difftest(Decode *_this, vaddr_t dnpc)
 
 static void exec_once(Decode *s)
 {
-    if (contextp->time() > 8750000000)
-    {
-        wave_enable = true;
-    }
     s->pc = top->io_pc;
     s->npc.inst.val = top->io_inst;
     static uint64_t skip_pc = 0;
