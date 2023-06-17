eb5f91905e8ff135cab764b09375a8e641947e1b
tracer-ysyx2205 <tracer@ysyx.org>
2023-06-17 17:38:36 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:38:36 up  9:14,  1 user,  load average: 1.05, 0.75, 0.48

--- npc/src/src/Cache.scala
+++ npc/src/src/Cache.scala
@@ -119,24 +119,24 @@ class Cache(way : Int) extends Module
     dirty := false.B
     hazard := false.B
 
-    val write_back_en = dontTouch(state === s_r && cnt === 3.U)
+    val refill_wen = dontTouch(state === s_r && cnt === 3.U)
     
     for (i <- 0 until way)
     {
-        ways(i).tag.io.cen  := (cache_ready && cpu_request) || (write_back_en && way_sel === i.U)
-        ways(i).tag.io.wen  := write_back_en && way_sel === i.U
+        ways(i).tag.io.cen  := (cache_ready && cpu_request) || (refill_wen && way_sel === i.U)
+        ways(i).tag.io.wen  := refill_wen && way_sel === i.U
         ways(i).tag.io.bwen := Fill(53, 1.U(1.W))
         ways(i).tag.io.A    := Mux(state === s_r, req_reg.index, req.index)
         ways(i).tag.io.D    := req_reg.tag
 
-        ways(i).V.io.cen   := (cache_ready && cpu_request) || (write_back_en && way_sel === i.U)
-        ways(i).V.io.wen   := write_back_en && way_sel === i.U
+        ways(i).V.io.cen   := (cache_ready && cpu_request) || (refill_wen && way_sel === i.U)
+        ways(i).V.io.wen   := refill_wen && way_sel === i.U
         ways(i).V.io.bwen  := 1.U(1.W)
         ways(i).V.io.A     := Mux(state === s_r, req_reg.index, req.index)
         ways(i).V.io.D     := 1.U(1.W)
 
-        ways(i).D.io.cen   := (state === s_lookup) || (write_back_en && way_sel === i.U)
-        ways(i).D.io.wen   := (state === s_lookup && hit_way(i) && req_reg.op) || (write_back_en && way_sel === i.U)
+        ways(i).D.io.cen   := (state === s_lookup) || (refill_wen && way_sel === i.U)
+        ways(i).D.io.wen   := (state === s_lookup && hit_way(i) && req_reg.op) || (refill_wen && way_sel === i.U)
         ways(i).D.io.bwen  := 1.U(1.W)
         ways(i).D.io.A     := Mux(state === s_r, req_reg.index, req.index)
         ways(i).D.io.D     := Mux(state === s_lookup && hit_way(i) && req_reg.op, 1.U(1.W), 0.U(1.W))
@@ -148,8 +148,8 @@ class Cache(way : Int) extends Module
                 bwen(k) := Fill(8, req_reg.strb)
             }
 
-            ways(i).data.banks(j).cen  := (cache_ready && cpu_request && req.offset(4, 3) === j.U) || (write_back_en && way_sel === i.U)
-            ways(i).data.banks(j).wen  := (state === s_lookup && hit_way(i) && req_reg.op && req_reg.offset(4, 3) === j.U) || (write_back_en && way_sel === i.U)
+            ways(i).data.banks(j).cen  := (cache_ready && cpu_request && req.offset(4, 3) === j.U) || (refill_wen && way_sel === i.U)
+            ways(i).data.banks(j).wen  := (state === s_lookup && hit_way(i) && req_reg.op && req_reg.offset(4, 3) === j.U) || (refill_wen && way_sel === i.U)
             ways(i).data.banks(j).bwen := Mux(state === s_r, Fill(64, 1.U(1.W)), bwen.asUInt())
             ways(i).data.banks(j).A    := Mux(state === s_r || state === s_lookup && hit_way(i) && req_reg.op && req_reg.offset(4, 3) === j.U, req_reg.index, req.index)
             ways(i).data.banks(j).D    := Mux(state === s_r, new_cache_line >> Cat(j.U, 0.U(6.W)), req_reg.data)
