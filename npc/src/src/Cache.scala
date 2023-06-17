9890bd1b98fb8d1ef6a96fd5821bbd860e123b96
tracer-ysyx2205 <tracer@ysyx.org>
2023-06-17 17:28:37 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:28:37 up  9:04,  1 user,  load average: 0.37, 0.37, 0.28

--- npc/src/src/Cache.scala
+++ npc/src/src/Cache.scala
@@ -135,7 +135,7 @@ class Cache(way : Int) extends Module
         ways(i).V.io.A     := Mux(state === s_r, req_reg.index, req.index)
         ways(i).V.io.D     := 1.U(1.W)
 
-        ways(i).D.io.cen   := (state === s_lookup) || (state === s_r && way_sel === i.U)
+        ways(i).D.io.cen   := (cache_ready && cpu_request) || (state === s_r && way_sel === i.U)
         ways(i).D.io.wen   := (state === s_lookup && hit_way(i) && req_reg.op) || (state === s_r && way_sel === i.U)
         ways(i).D.io.bwen  := 1.U(1.W)
         ways(i).D.io.A     := Mux(state === s_r, req_reg.index, req.index)
