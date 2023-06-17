6b486cd32fd8c069297dd47d8fed5396f2d4f3da
tracer-ysyx2205 <tracer@ysyx.org>
2023-06-17 15:02:14 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:02:14 up  6:37,  1 user,  load average: 0.00, 0.00, 0.00

--- npc/src/src/PF.scala
+++ npc/src/src/PF.scala
@@ -37,14 +37,19 @@ class PF extends Module
     inst_master.w.bits.data := 0.U(64.W)
     inst_master.w.bits.strb := 0.U(8.W)
 
-    when (ds_pf.ds_allow_in)
-    {
+    when (inst_master.ar.fire) {
+        arfire := true.B
+    } .elsewhen (ds_pf.ds_allow_in) {
         arfire := false.B
     }
-    .elsewhen (inst_master.ar.fire)
-    {
-        arfire := true.B
-    }
+    // when (ds_pf.ds_allow_in)
+    // {
+    //     arfire := false.B
+    // }
+    // .elsewhen (inst_master.ar.fire)
+    // {
+    //     arfire := true.B
+    // }
 
     pf_fs.to_fs_valid := to_fs_valid
     pf_fs.next_pc := next_pc
