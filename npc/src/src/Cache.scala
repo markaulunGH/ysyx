7b7f81a4aae4a83feb9bf81b337f2dd681a2136d
tracer-ysyx2205 <tracer@ysyx.org>
2023-06-17 09:45:49 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:45:49 up  1:21,  1 user,  load average: 0.43, 0.34, 0.21

--- npc/src/src/Cache.scala
+++ npc/src/src/Cache.scala
@@ -15,7 +15,9 @@ class Cache_Sram(width : Int, depth : Int) extends Module
     })
 
     val ram = RegInit(VecInit(Seq.fill(depth)(0.U(width.W))))
-    ram(io.A) := RegEnable((io.D & io.bwen) | (ram(io.A) & ~io.bwen), io.cen && io.wen)
+    when (io.cen && io.wen) {
+        ram(io.A) := (io.D & io.bwen) | (ram(io.A) & ~io.bwen)
+    }
     io.Q := RegEnable(ram(io.A), io.cen && !io.wen)
 }
 
