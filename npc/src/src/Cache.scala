17a60c7c7556dd94b55b664a09a053b084ae616c
tracer-ysyx2205 <tracer@ysyx.org>
2023-06-17 17:20:37 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:20:37 up  8:56,  1 user,  load average: 0.77, 0.36, 0.27

--- npc/src/src/Cache.scala
+++ npc/src/src/Cache.scala
@@ -101,6 +101,8 @@ class Cache(way : Int) extends Module
         s_r      -> Mux(slave.r.fire, Mux(cnt === 3.U, Mux(cpu_ready, s_idle, s_wait), s_ar), s_r)
     ))
 
+    val cache_ready = dontTouch(state === s_idle || (state === s_lookup && hit))
+
     val req_reg = RegEnable(req, (state === s_idle || (state === s_lookup && hit)) && cpu_request && !hazard)
     val way_sel = RegEnable(random_bit(log2Ceil(way) - 1, 0), state === s_lookup)
 
