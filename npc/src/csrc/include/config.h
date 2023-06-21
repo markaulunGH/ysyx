052e45003107e4dd0c7c9c6ee085b234eb71fb84
tracer-ysyx2205 <tracer@ysyx.org>
2023-06-21 23:57:53 +0800

    > generate verilog ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:57:53 up  1:28,  0 users,  load average: 0.21, 0.39, 0.30

--- npc/src/csrc/include/config.h
+++ npc/src/csrc/include/config.h
@@ -1,13 +1,13 @@
 #ifndef __CONFIG_H__
 #define __CONFIG_H__
 
-#define CONFIG_WAVE
+// #define CONFIG_WAVE
 // #define CONFIG_WAVE_FST
 // #define CONFIG_ITRACE
 // #define CONFIG_ITRACE_RING
 // #define CONFIG_FTRACE
 // #define CONFIG_MTRACE
 // #define CONFIG_DTRACE
-#define CONFIG_DIFFTEST
+// #define CONFIG_DIFFTEST
 
 #endif
