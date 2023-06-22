b938c7878c72d75b796525e3b67f25aed83a88d4
tracer-ysyx2205 <tracer@ysyx.org>
2023-06-22 22:11:56 +0800

    > sim RTL ysyx_22051094 李衍君 Linux Muzi 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:11:56 up 13:50,  1 user,  load average: 0.29, 0.39, 0.80

--- npc/src/csrc/cpu/cpu-exec.cpp
+++ npc/src/csrc/cpu/cpu-exec.cpp
@@ -266,7 +266,7 @@ static void exec_once(Decode *s)
     s->dnpc = top->io_pc;
     cpu.pc = s->dnpc;
     printf("pc = %lx\n", s->dnpc);
-    printf("%d\n", contextp->time());
+    printf("%ld\n", contextp->time());
 #ifdef CONFIG_ITRACE
     char *p = s->logbuf;
     p += snprintf(p, sizeof(s->logbuf), "0x%016lx:", s->pc);
