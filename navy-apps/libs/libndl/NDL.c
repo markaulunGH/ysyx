b0bce57f69e8be522282752444fb38448a0e13e3
ysyx_22051094-Li Yanjun <liyanjun20@mails.ucas.ac.cn>
2023-06-29 09:55:38 +0800

    fix: update include file

--- navy-apps/libs/libndl/NDL.c
+++ navy-apps/libs/libndl/NDL.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <sys/time.h>
+#include <sys/file.h>
 
 static int evtdev = -1;
 static int fbdev = -1;
@@ -10,7 +12,7 @@ static int screen_w = 0, screen_h = 0;
 
 uint32_t NDL_GetTicks() {
   struct timeval tv;
-  _gettimeofday(&tv, NULL);
+  gettimeofday(&tv, NULL);
   return tv.tv_sec * 1000 + tv.tv_usec / 1000;
 }
 
